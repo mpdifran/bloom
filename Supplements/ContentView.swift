7550b622871bf6dafc45da269d341a52819dd830
Mark DiFranco <mpdifranco@icloud.com>
2024-03-21 10:11:24 -0400

    Initial Commit

---
+++ Supplements/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Supplements
+//
+//  Created by Mark DiFranco on 2024-03-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Supplements/SupplementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupplementsApp.swift
+//  Supplements
+//
+//  Created by Mark DiFranco on 2024-03-21.
+//
+
+import SwiftUI
+
+@main
+struct SupplementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
