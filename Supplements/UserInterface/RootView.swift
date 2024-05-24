2802798e20abf9e7278a962403773161f392f7b1
Mark DiFranco <mpdifranco@icloud.com>
2024-05-23 20:40:18 -0400

    Select chat tab when handling a notification

--- Supplements/UserInterface/RootView.swift
+++ Supplements/UserInterface/RootView.swift
@@ -7,9 +7,12 @@
 
 import SwiftUI
 
+@MainActor
 struct RootView: View {
     
     @AppStorage("hasShownOnboardingV2") var hasShownOnboarding: Bool = false
+
+    @StateObject private var tabController = TabController()
 
     var body: some View {
         Group {
@@ -21,11 +24,15 @@ struct RootView: View {
                     }
                 }
             } else {
-                TabView {
+                TabView(selection: $tabController.activeTab) {
                     InsightsView()
+                        .tag(Tab.insights)
                     ChatView()
+                        .tag(Tab.chat)
                     ProfileView()
+                        .tag(Tab.profile)
                 }
+                .environmentObject(tabController)
             }
         }
         .animation(.easeInOut(duration: 1), value: hasShownOnboarding)
---
+++ Supplements/UserInterface/Tab/TabController.swift
@@ -0,0 +1,47 @@
+//
+//  TabController.swift
+//  Supplements
+//
+//  Created by Mark DiFranco on 2024-05-23.
+//
+
+import SwiftUI
+import UserNotifications
+
+enum Tab {
+    case insights
+    case chat
+    case profile
+}
+
+@MainActor
+class TabController: NSObject, ObservableObject {
+    @Published var activeTab = Tab.insights
+
+    override init() {
+        super.init()
+
+        UNUserNotificationCenter.current().delegate = self
+    }
+}
+
+extension TabController {
+
+    func select(_ tab: Tab) {
+        activeTab = tab
+    }
+}
+
+extension TabController: UNUserNotificationCenterDelegate {
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse
+    ) async {
+        guard response.notification.request.identifier == "chat-message" else { return }
+
+        await MainActor.run {
+            select(.chat)
+        }
+    }
+}
