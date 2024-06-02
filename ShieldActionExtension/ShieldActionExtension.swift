2b470dc7934f64c37e79b9cc7066c1b147d4574a
Mark DiFranco <mpdifranco@icloud.com>
2024-06-02 16:23:18 -0400

    Update button action handlers

--- ShieldActionExtension/ShieldActionExtension.swift
+++ ShieldActionExtension/ShieldActionExtension.swift
@@ -10,7 +10,6 @@ import ManagedSettings
 class ShieldActionExtension: ShieldActionDelegate {
 
     override func handle(action: ShieldAction, for application: ApplicationToken, completionHandler: @escaping (ShieldActionResponse) -> Void) {
-        // Handle the action as needed.
         switch action {
         case .primaryButtonPressed:
             completionHandler(.close)
@@ -22,12 +21,24 @@ class ShieldActionExtension: ShieldActionDelegate {
     }
     
     override func handle(action: ShieldAction, for webDomain: WebDomainToken, completionHandler: @escaping (ShieldActionResponse) -> Void) {
-        // Handle the action as needed.
-        completionHandler(.close)
+        switch action {
+        case .primaryButtonPressed:
+            completionHandler(.close)
+        case .secondaryButtonPressed:
+            completionHandler(.defer)
+        @unknown default:
+            fatalError()
+        }
     }
     
     override func handle(action: ShieldAction, for category: ActivityCategoryToken, completionHandler: @escaping (ShieldActionResponse) -> Void) {
-        // Handle the action as needed.
-        completionHandler(.close)
+        switch action {
+        case .primaryButtonPressed:
+            completionHandler(.close)
+        case .secondaryButtonPressed:
+            completionHandler(.defer)
+        @unknown default:
+            fatalError()
+        }
     }
 }
