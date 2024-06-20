5ff9781cb19ed62d55c76e57af436019a80849f0
Mark DiFranco <mpdifranco@icloud.com>
2024-06-20 09:33:35 -0400

    Only play sounds when the app is active

--- Supplements/Utilities/SoundPlayer.swift
+++ Supplements/Utilities/SoundPlayer.swift
@@ -5,17 +5,21 @@
 //  Created by Mark DiFranco on 2024-05-12.
 //
 
-import Foundation
+import UIKit
 import AVFoundation
 
 /// Sounds can be found here: https://github.com/TUNER88/iOSSystemSoundsLibrary
 struct SoundPlayer {
 
     static func playSendMessage() {
+        guard UIApplication.shared.applicationState == .active else { return }
+
         AudioServicesPlaySystemSound(1004)
     }
 
     static func playReceiveMessage() {
+        guard UIApplication.shared.applicationState == .active else { return }
+
         AudioServicesPlaySystemSound(1003)
     }
 }
