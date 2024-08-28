d789903553a9618f1b47d5e2c5575fb2344c42e6
Mark DiFranco <mpdifranco@icloud.com>
2024-08-27 21:51:44 -0400

    Update bowel movement logger

--- Supplements/UserInterface/Actions/ActionCards/BowelMovementActionCardView.swift
+++ Supplements/UserInterface/Actions/ActionCards/BowelMovementActionCardView.swift
@@ -10,41 +10,136 @@ import SwiftData
 
 struct BowelMovementActionCardView: View {
     @State private var date = Date.now
-    @State private var stoolType: Int = 1
+    @State private var selectedStoolType: Int = 0
     @State private var duration: BowelMovement.Duration = .between5And10Min
 
     var body: some View {
         ActionCardView(title: "New Bowel Movement") { modelContext in
             let model = BowelMovement(
                 date: date,
-                bristolStoolType: stoolType,
+                bristolStoolType: selectedStoolType,
                 duration: duration
             )
             modelContext.insert(model)
             return true
         } content: { (_, _) in
-            List {
-                Section {
-                    Picker("Stool Type", selection: $stoolType) {
-                        ForEach(1...7, id: \.self) { stoolType in
-                            Text("Type \(stoolType)")
-                                .tag(stoolType)
+            ScrollView {
+                ScrollViewReader { proxy in
+                    ScrollView(.horizontal) {
+                        HStack {
+                            VStack {
+                                Spacer(minLength: 0)
+                                Text("Unknown")
+                                    .font(.subheadline)
+                                    .bold()
+                                Spacer(minLength: 0)
+                                Image(systemName: "questionmark.app.fill")
+                                    .font(.largeTitle)
+
+                                Spacer(minLength: 0)
+                            }
+                            .frame(width: 130)
+                            .padding()
+                            .background {
+                                RoundedRectangle(cornerRadius: 13)
+                                    .fill(.background.secondary)
+                            }
+                            .overlay {
+                                if selectedStoolType == 0 {
+                                    RoundedRectangle(cornerRadius: 13)
+                                        .stroke(.tint, lineWidth: 3)
+                                }
+                            }
+                            .id(0)
+                            .onTapGesture {
+                                selectedStoolType = 0
+                            }
+
+                            ForEach(1...7, id: \.self) { stoolType in
+                                VStack {
+                                    Text("Type \(stoolType)")
+                                        .font(.subheadline)
+                                        .bold()
+                                    Image("Type \(stoolType)")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(height: 50)
+                                        .clipShape(RoundedRectangle(cornerRadius: 13))
+
+                                    Spacer()
+
+                                    Text(description(for: stoolType))
+                                        .font(.subheadline)
+                                        .fixedSize(horizontal: false, vertical: true)
+                                        .multilineTextAlignment(.center)
+                                }
+                                .frame(width: 130)
+                                .padding()
+                                .background {
+                                    RoundedRectangle(cornerRadius: 13)
+                                        .fill(.background.secondary)
+                                }
+                                .overlay {
+                                    if selectedStoolType == stoolType {
+                                        RoundedRectangle(cornerRadius: 13)
+                                            .stroke(.tint, lineWidth: 3)
+                                    }
+                                }
+                                .onTapGesture {
+                                    selectedStoolType = stoolType
+                                }
+                            }
+                        }
+                        .padding()
+                    }
+                    .scrollIndicators(.never)
+                    .onChange(of: selectedStoolType) { (_, newValue) in
+                        withAnimation {
+                            proxy.scrollTo(newValue, anchor: .center)
                         }
                     }
-                    Picker("Duration", selection: $duration) {
+                }
+                .sensoryFeedback(.selection, trigger: selectedStoolType)
+
+                LabeledContent("Duration") {
+                    Picker("", selection: $duration) {
                         ForEach(BowelMovement.Duration.allCases) { duration in
                             Text(duration.name)
                                 .tag(duration)
                         }
                     }
                 }
+                .padding(.horizontal)
+                .padding(.vertical, 12)
+                .cardContainer(fill: .background.secondary, includePadding: false)
+                .padding(.horizontal)
 
-                Section {
-                    DatePicker("Time", selection: $date)
+                LabeledContent("Date") {
+                    DatePicker("", selection: $date)
                 }
+                .padding(.horizontal)
+                .padding(.vertical, 12)
+                .cardContainer(fill: .background.secondary, includePadding: false)
+                .padding(.horizontal)
             }
         }
         .tint(.brown)
+    }
+}
+
+private extension BowelMovementActionCardView {
+
+    func description(for type: Int) -> String {
+        switch type {
+        case 1: "Separate hard lumps, hard to pass"
+        case 2: "Sausage shape but lumpy"
+        case 3: "Like a sausage but with cracks"
+        case 4: "Like a sausage, smooth and long"
+        case 5: "Soft blobs with clear cut edges"
+        case 6: "Mushy consistency with ragged edges"
+        case 7: "Liquid consistency with no solid pieces"
+        default: ""
+        }
     }
 }
 
