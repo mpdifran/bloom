6087c22719bbdff996c9cc8e932d7a02d77936f6
Mark DiFranco <mpdifranco@icloud.com>
2024-12-02 16:51:03 -0500

    Write all nutrients to Apple Health

--- Apps/Bloom/DataContainer/SwiftData/Schema/DTOs/FoodItem+DTO.swift
+++ Apps/Bloom/DataContainer/SwiftData/Schema/DTOs/FoodItem+DTO.swift
@@ -9,80 +9,164 @@ import Foundation
 import SwiftData
 
 public struct FoodItemDTO: Sendable {
-    public let persistentID: PersistentIdentifier
-    public let id: String
-    public let name: String
-    public let brandName: String
-    public let flavour: String
-    public let calories: Double
-    public let protein: Double
-    public let carbohydrates: Double
-    public let fat: Double
-    public let servingName: String?
-    public let servingUnitString: String?
-    public let servingValue: Double?
-    public let ingredients: String?
-    public let category: FoodItemRecord.Category?
-    public let isVerified: Bool
-    public let foodItemLogIDs: [String]
+  public let persistentID: PersistentIdentifier
+  public let id: String
+  public let name: String
+  public let brandName: String
+  public let flavour: String
 
-    public init(
-        persistentID: PersistentIdentifier,
-        id: String,
-        name: String,
-        brandName: String,
-        flavour: String,
-        calories: Double,
-        protein: Double,
-        carbohydrates: Double,
-        fat: Double,
-        servingName: String?,
-        servingUnitString: String?,
-        servingValue: Double?,
-        ingredients: String?,
-        category: FoodItemRecord.Category?,
-        isVerified: Bool,
-        foodItemLogIDs: [String]
-    ) {
-        self.persistentID = persistentID
-        self.id = id
-        self.name = name
-        self.brandName = brandName
-        self.flavour = flavour
-        self.calories = calories
-        self.protein = protein
-        self.carbohydrates = carbohydrates
-        self.fat = fat
-        self.servingName = servingName
-        self.servingUnitString = servingUnitString
-        self.servingValue = servingValue
-        self.ingredients = ingredients
-        self.category = category
-        self.isVerified = isVerified
-        self.foodItemLogIDs = foodItemLogIDs
-    }
+  public let rawCountry: String?
+
+  public let calories: Double
+  public let protein: Double
+  public let carbohydrates: Double
+  public let fat: Double
+
+  public var saturatedFat: Double?
+  public var transFat: Double?
+  public var polyunsaturatedFat: Double?
+  public var monounsaturatedFat: Double?
+  public var fiber: Double?
+  public var sugar: Double?
+  public var cholesterol: Double?
+  public var sodium: Double?
+  public var calcium: Double?
+  public var iron: Double?
+  public var potassium: Double?
+  public var magnesium: Double?
+  public var zinc: Double?
+  public var vitaminA: Double?
+  public var vitaminB6: Double?
+  public var vitaminB12: Double?
+  public var vitaminC: Double?
+  public var vitaminD: Double?
+  public var vitaminE: Double?
+
+  public let servingName: String?
+  public let servingUnitString: String?
+  public let servingValue: Double?
+  public let ingredients: String?
+  public let category: FoodItemRecord.Category?
+  public let isVerified: Bool
+  public let foodItemLogIDs: [String]
+
+  public init(
+    persistentID: PersistentIdentifier,
+    id: String,
+    name: String,
+    brandName: String,
+    flavour: String,
+    rawCountry: String?,
+    calories: Double,
+    protein: Double,
+    carbohydrates: Double,
+    fat: Double,
+    saturatedFat: Double?,
+    transFat: Double?,
+    polyunsaturatedFat: Double?,
+    monounsaturatedFat: Double?,
+    fiber: Double?,
+    sugar: Double?,
+    cholesterol: Double?,
+    sodium: Double?,
+    calcium: Double?,
+    iron: Double?,
+    potassium: Double?,
+    magnesium: Double?,
+    zinc: Double?,
+    vitaminA: Double?,
+    vitaminB6: Double?,
+    vitaminB12: Double?,
+    vitaminC: Double?,
+    vitaminD: Double?,
+    vitaminE: Double?,
+    servingName: String?,
+    servingUnitString: String?,
+    servingValue: Double?,
+    ingredients: String?,
+    category: FoodItemRecord.Category?,
+    isVerified: Bool,
+    foodItemLogIDs: [String]
+  ) {
+    self.persistentID = persistentID
+    self.id = id
+    self.name = name
+    self.brandName = brandName
+    self.flavour = flavour
+    self.rawCountry = rawCountry
+    self.calories = calories
+    self.protein = protein
+    self.carbohydrates = carbohydrates
+    self.fat = fat
+    self.saturatedFat = saturatedFat
+    self.transFat = transFat
+    self.polyunsaturatedFat = polyunsaturatedFat
+    self.monounsaturatedFat = monounsaturatedFat
+    self.fiber = fiber
+    self.sugar = sugar
+    self.cholesterol = cholesterol
+    self.sodium = sodium
+    self.calcium = calcium
+    self.iron = iron
+    self.potassium = potassium
+    self.magnesium = magnesium
+    self.zinc = zinc
+    self.vitaminA = vitaminA
+    self.vitaminB6 = vitaminB6
+    self.vitaminB12 = vitaminB12
+    self.vitaminC = vitaminC
+    self.vitaminD = vitaminD
+    self.vitaminE = vitaminE
+    self.servingName = servingName
+    self.servingUnitString = servingUnitString
+    self.servingValue = servingValue
+    self.ingredients = ingredients
+    self.category = category
+    self.isVerified = isVerified
+    self.foodItemLogIDs = foodItemLogIDs
+  }
 }
 
 public extension FoodItemRecord {
 
-    func asDTO() -> FoodItemDTO {
-        FoodItemDTO(
-            persistentID: persistentModelID,
-            id: id,
-            name: name,
-            brandName: brandName,
-            flavour: flavour,
-            calories: calories,
-            protein: protein,
-            carbohydrates: carbohydrates,
-            fat: fat,
-            servingName: servingName,
-            servingUnitString: servingUnitString,
-            servingValue: servingValue,
-            ingredients: ingredients,
-            category: category,
-            isVerified: isVerified,
-            foodItemLogIDs: logs?.map({ $0.id }) ?? []
-        )
-    }
+  func asDTO() -> FoodItemDTO {
+    FoodItemDTO(
+      persistentID: persistentModelID,
+      id: id,
+      name: name,
+      brandName: brandName,
+      flavour: flavour,
+      rawCountry: rawCountry,
+      calories: calories,
+      protein: protein,
+      carbohydrates: carbohydrates,
+      fat: fat,
+      saturatedFat: saturatedFat,
+      transFat: transFat,
+      polyunsaturatedFat: polyunsaturatedFat,
+      monounsaturatedFat: monounsaturatedFat,
+      fiber: fiber,
+      sugar: sugar,
+      cholesterol: cholesterol,
+      sodium: sodium,
+      calcium: calcium,
+      iron: iron,
+      potassium: potassium,
+      magnesium: magnesium,
+      zinc: zinc,
+      vitaminA: vitaminA,
+      vitaminB6: vitaminB6,
+      vitaminB12: vitaminB12,
+      vitaminC: vitaminC,
+      vitaminD: vitaminD,
+      vitaminE: vitaminE,
+      servingName: servingName,
+      servingUnitString: servingUnitString,
+      servingValue: servingValue,
+      ingredients: ingredients,
+      category: category,
+      isVerified: isVerified,
+      foodItemLogIDs: logs?.map({ $0.id }) ?? []
+    )
+  }
 }
--- Apps/Bloom/Supplements/HealthManager/HealthStoreModifier.swift
+++ Apps/Bloom/Supplements/HealthManager/HealthStoreModifier.swift
@@ -12,109 +12,114 @@ import DataContainer
 // MARK: - HealthStoreModifier
 
 final actor HealthStoreModifier {
-    static let shared = HealthStoreModifier()
+  static let shared = HealthStoreModifier()
 
-    private let healthStore = HKHealthStore()
-    private let foodItemLogModel = FoodItemLogModelActor(modelContainer: ContainerHolder.shared.container)
+  private let healthStore = HKHealthStore()
+  private let foodItemLogModel = FoodItemLogModelActor(modelContainer: ContainerHolder.shared.container)
 
-    private init() { }
+  private init() { }
 }
 
 // MARK: Generic Writes
 
 extension HealthStoreModifier {
 
-    func write(sample: HKObject) async throws {
-        try await healthStore.save(sample)
-    }
+  func write(sample: HKObject) async throws {
+    try await healthStore.save(sample)
+  }
 
-    func write(samples: [HKObject]) async throws {
-        try await healthStore.save(samples)
-    }
+  func write(samples: [HKObject]) async throws {
+    try await healthStore.save(samples)
+  }
 }
 
 // MARK: Nutrition
 
 extension HealthStoreModifier {
 
-    func updateNutrition(for date: Date) async throws {
-        // Fetch logs from local database.
-        let foodLogs = try await foodItemLogModel.fetchLogs(for: date)
-        // Fetch and delete all entries for the day.
-        try await clearExistingEntries(for: date)
-        // Write food logs to HealthKit.
-        try await recordFoodLogs(foodLogs)
-    }
+  func updateNutrition(for date: Date) async throws {
+    // Fetch logs from local database.
+    let foodLogs = try await foodItemLogModel.fetchLogs(for: date)
+    // Fetch and delete all entries for the day.
+    try await clearExistingEntries(for: date)
+    // Write food logs to HealthKit.
+    try await recordFoodLogs(foodLogs)
+  }
 }
 
 private extension HealthStoreModifier {
-    func clearExistingEntries(for date: Date) async throws {
-        // We need to find and delete for each quantity type. ex calories, protein, carbs, fat.
-        for sampleType in FoodItemNutrient.allCases {
-            do {
-                let type = HKQuantityType.quantityType(forIdentifier: sampleType.identifier)!
-                let samples = await HealthStoreFetcher.shared.fetchSamples(
-                    for: type,
-                    dateRange: .duringDay(date),
-                    writtenByApp: true
-                )
-                guard samples.isNotEmpty else { return }
-                // Cast to the correct type for delete. HKSamples are also HKObjects.
-                let objects = samples as [HKObject]
-                try await healthStore.delete(objects)
-            } catch {
-                print("Error deleting samples for \(sampleType.identifier): \(error.localizedDescription)")
-            }
-        }
-    }
-
-    func recordFoodLogs(_ foodItemLogs: [FoodItemLogDTO]) async throws {
-        // Each log will make entries for each log, for the samples: calories, protein, carbs, and fat.
-        // These will be batched, and logged once.
-        let samples = foodItemLogs.flatMap { log in
-            createFoodSamples(log)
-        }
-
+  func clearExistingEntries(for date: Date) async throws {
+    // We need to find and delete for each quantity type. ex calories, protein, carbs, fat.
+    for sampleType in FoodItemNutrient.allCases {
+      do {
+        let type = HKQuantityType.quantityType(forIdentifier: sampleType.identifier)!
+        let samples = await HealthStoreFetcher.shared.fetchSamples(
+          for: type,
+          dateRange: .duringDay(date),
+          writtenByApp: true
+        )
         guard samples.isNotEmpty else { return }
-        // Write to HealthKit
-        try await write(samples: samples)
+        // Cast to the correct type for delete. HKSamples are also HKObjects.
+        let objects = samples as [HKObject]
+        try await healthStore.delete(objects)
+      } catch {
+        print("Error deleting samples for \(sampleType.identifier): \(error.localizedDescription)")
+      }
+    }
+  }
+
+  func recordFoodLogs(_ foodItemLogs: [FoodItemLogDTO]) async throws {
+    // Each log will make entries for each log, for the samples: calories, protein, carbs, and fat.
+    // These will be batched, and logged once.
+    let samples = foodItemLogs.flatMap { log in
+      createFoodSamples(log)
     }
 
-    func createFoodSamples(_ foodItemLog: FoodItemLogDTO) -> [HKQuantitySample] {
-        FoodItemNutrient.allCases.compactMap { type in
-            createFoodSample(
-                foodItemLog,
-                type: type
-            )
-        }
+    guard samples.isNotEmpty else { return }
+    // Write to HealthKit
+    try await write(samples: samples)
+  }
+
+  func createFoodSamples(_ foodItemLog: FoodItemLogDTO) -> [HKQuantitySample] {
+    FoodItemNutrient.allCases.compactMap { type in
+      createFoodSample(
+        foodItemLog,
+        type: type
+      )
     }
+  }
 
-    func createFoodSample(
-        _ foodItemLog: FoodItemLogDTO,
-        type: FoodItemNutrient
-    ) -> HKQuantitySample? {
-        guard let foodItem = foodItemLog.foodItem else { return nil }
+  func createFoodSample(
+    _ foodItemLog: FoodItemLogDTO,
+    type: FoodItemNutrient
+  ) -> HKQuantitySample? {
+    guard
+      let foodItem = foodItemLog.foodItem,
+      let amount = type.value(for: foodItem),
+      amount > 0
+    else { return nil }
 
-        let totalValue = foodItemLog.numberOfServings * type.value(for: foodItem)
 
-        let quantity = HKQuantity(
-            unit: type.unit,
-            doubleValue: totalValue
-        )
+    let totalValue = foodItemLog.numberOfServings * amount
 
-        let metaData = HealthMetadata.create(
-            [
-                .food(foodItem.displayFullName),
-                .meal(foodItemLog.meal.name)
-            ]
-        )
+    let quantity = HKQuantity(
+      unit: type.unit,
+      doubleValue: totalValue
+    )
 
-        return HKQuantitySample(
-            type: .quantityType(forIdentifier: type.identifier)!,
-            quantity: quantity,
-            start: foodItemLog.date,
-            end: foodItemLog.date,
-            metadata: metaData
-        )
-    }
+    let metaData = HealthMetadata.create(
+      [
+        .food(foodItem.displayFullName),
+        .meal(foodItemLog.meal.name)
+      ]
+    )
+
+    return HKQuantitySample(
+      type: .quantityType(forIdentifier: type.identifier)!,
+      quantity: quantity,
+      start: foodItemLog.date,
+      end: foodItemLog.date,
+      metadata: metaData
+    )
+  }
 }
