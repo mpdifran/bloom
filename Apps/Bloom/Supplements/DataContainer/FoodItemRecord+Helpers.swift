c817934f377a1f46fde9478e4a82f6a08bf1616c
Mark DiFranco <mpdifranco@icloud.com>
2024-12-02 19:26:18 -0500

    Update local food item when logging

--- Apps/Bloom/Supplements/DataContainer/FoodItemRecord+Helpers.swift
+++ Apps/Bloom/Supplements/DataContainer/FoodItemRecord+Helpers.swift
@@ -10,100 +10,137 @@ import BloomModel
 
 extension FoodItemRecord {
 
-    convenience init(foodItem: FoodItem) {
-        self.init(
-            id: foodItem.id.value,
-            name: foodItem.name,
-            brandName: foodItem.brandName ?? "",
-            flavour: foodItem.flavour ?? "",
-            rawCountry: foodItem.country?.rawValue,
-            calories: foodItem.calories?.value ?? 0,
-            protein: foodItem.protein?.value ?? 0,
-            carbohydrates: foodItem.carbohydrates?.value ?? 0,
-            fat: foodItem.fat?.value ?? 0,
-            saturatedFat: foodItem.saturatedFat?.value,
-            transFat: foodItem.transFat?.value,
-            polyunsaturatedFat: foodItem.polyunsaturatedFat?.value,
-            monounsaturatedFat: foodItem.monounsaturatedFat?.value,
-            fiber: foodItem.fiber?.value,
-            sugar: foodItem.sugar?.value,
-            cholesterol: foodItem.cholesterol?.value,
-            sodium: foodItem.sodium?.value,
-            calcium: foodItem.calcium?.value,
-            iron: foodItem.iron?.value,
-            potassium: foodItem.potassium?.value,
-            magnesium: foodItem.magnesium?.value,
-            zinc: foodItem.zinc?.value,
-            vitaminA: foodItem.vitaminA?.value,
-            vitaminB6: foodItem.vitaminB6?.value,
-            vitaminB12: foodItem.vitaminB12?.value,
-            vitaminC: foodItem.vitaminC?.value,
-            vitaminD: foodItem.vitaminD?.value,
-            vitaminE: foodItem.vitaminE?.value,
-            servingName: foodItem.servingName,
-            servingUnitString: foodItem.servingQuantity?.unit,
-            servingValue: foodItem.servingQuantity?.value,
-            ingredients: foodItem.ingredients,
-            category: .init(rawValue: foodItem.category.rawValue),
-            isVerified: foodItem.isVerified,
-            logs: []
-        )
-    }
+  convenience init(foodItem: FoodItem) {
+    self.init(
+      id: foodItem.id.value,
+      name: foodItem.name,
+      brandName: foodItem.brandName ?? "",
+      flavour: foodItem.flavour ?? "",
+      rawCountry: foodItem.country?.rawValue,
+      calories: foodItem.calories?.value ?? 0,
+      protein: foodItem.protein?.value ?? 0,
+      carbohydrates: foodItem.carbohydrates?.value ?? 0,
+      fat: foodItem.fat?.value ?? 0,
+      saturatedFat: foodItem.saturatedFat?.value,
+      transFat: foodItem.transFat?.value,
+      polyunsaturatedFat: foodItem.polyunsaturatedFat?.value,
+      monounsaturatedFat: foodItem.monounsaturatedFat?.value,
+      fiber: foodItem.fiber?.value,
+      sugar: foodItem.sugar?.value,
+      cholesterol: foodItem.cholesterol?.value,
+      sodium: foodItem.sodium?.value,
+      calcium: foodItem.calcium?.value,
+      iron: foodItem.iron?.value,
+      potassium: foodItem.potassium?.value,
+      magnesium: foodItem.magnesium?.value,
+      zinc: foodItem.zinc?.value,
+      vitaminA: foodItem.vitaminA?.value,
+      vitaminB6: foodItem.vitaminB6?.value,
+      vitaminB12: foodItem.vitaminB12?.value,
+      vitaminC: foodItem.vitaminC?.value,
+      vitaminD: foodItem.vitaminD?.value,
+      vitaminE: foodItem.vitaminE?.value,
+      servingName: foodItem.servingName,
+      servingUnitString: foodItem.servingQuantity?.unit,
+      servingValue: foodItem.servingQuantity?.value,
+      ingredients: foodItem.ingredients,
+      category: .init(rawValue: foodItem.category.rawValue),
+      isVerified: foodItem.isVerified,
+      logs: []
+    )
+  }
+
+  func apply(foodItem: FoodItem) {
+    self.id = foodItem.id.value
+    self.name = foodItem.name
+    self.brandName = foodItem.brandName ?? ""
+    self.flavour = foodItem.flavour ?? ""
+    self.rawCountry = foodItem.country?.rawValue
+    self.calories = foodItem.calories?.value ?? 0
+    self.protein = foodItem.protein?.value ?? 0
+    self.carbohydrates = foodItem.carbohydrates?.value ?? 0
+    self.fat = foodItem.fat?.value ?? 0
+    self.saturatedFat = foodItem.saturatedFat?.value
+    self.transFat = foodItem.transFat?.value
+    self.polyunsaturatedFat = foodItem.polyunsaturatedFat?.value
+    self.monounsaturatedFat = foodItem.monounsaturatedFat?.value
+    self.fiber = foodItem.fiber?.value
+    self.sugar = foodItem.sugar?.value
+    self.cholesterol = foodItem.cholesterol?.value
+    self.sodium = foodItem.sodium?.value
+    self.calcium = foodItem.calcium?.value
+    self.iron = foodItem.iron?.value
+    self.potassium = foodItem.potassium?.value
+    self.magnesium = foodItem.magnesium?.value
+    self.zinc = foodItem.zinc?.value
+    self.vitaminA = foodItem.vitaminA?.value
+    self.vitaminB6 = foodItem.vitaminB6?.value
+    self.vitaminB12 = foodItem.vitaminB12?.value
+    self.vitaminC = foodItem.vitaminC?.value
+    self.vitaminD = foodItem.vitaminD?.value
+    self.vitaminE = foodItem.vitaminE?.value
+    self.servingName = foodItem.servingName
+    self.servingUnitString = foodItem.servingQuantity?.unit
+    self.servingValue = foodItem.servingQuantity?.value
+    self.ingredients = foodItem.ingredients
+    self.category = .init(rawValue: foodItem.category.rawValue)
+    self.isVerified = foodItem.isVerified
+  }
 }
 
 extension FoodItemRecord {
 
-    func asNetworkFoodItem() -> FoodItem {
-        let quantity: FoodItem.Quantity?
-        if let servingValue, let servingUnitString {
-            quantity = .init(
-                value: servingValue,
-                unit: servingUnitString
-            )
-        } else {
-            quantity = nil
-        }
-
-        return FoodItem(
-            id: .init(id),
-            name: name,
-            brandName: brandName,
-            flavour: flavour,
-            country: FoodItem.Country(rawValue: rawCountry ?? ""),
-            calories: .init(value: calories, unit: "kcal"),
-            protein: .init(value: protein, unit: "g"),
-            carbohydrates: .init(value: carbohydrates, unit: "g"),
-            fat: .init(value: fat, unit: "g"),
-            saturatedFat: saturatedFat.map({ .init(value: $0, unit: "g") }),
-            transFat: transFat.map({ .init(value: $0, unit: "g") }),
-            polyunsaturatedFat: polyunsaturatedFat.map({ .init(value: $0, unit: "g") }),
-            monounsaturatedFat: monounsaturatedFat.map({ .init(value: $0, unit: "g") }),
-            fiber: fiber.map({ .init(value: $0, unit: "g") }),
-            sugar: sugar.map({ .init(value: $0, unit: "g") }),
-            cholesterol: cholesterol.map({ .init(value: $0, unit: "mg") }),
-            sodium: sodium.map({ .init(value: $0, unit: "mg") }),
-            calcium: calcium.map({ .init(value: $0, unit: "mg") }),
-            iron: iron.map({ .init(value: $0, unit: "mg") }),
-            potassium: potassium.map({ .init(value: $0, unit: "mg") }),
-            magnesium: magnesium.map({ .init(value: $0, unit: "mg") }),
-            zinc: zinc.map({ .init(value: $0, unit: "mg") }),
-            vitaminA: vitaminA.map({ .init(value: $0, unit: "mg") }),
-            vitaminB6: vitaminB6.map({ .init(value: $0, unit: "mg") }),
-            vitaminB12: vitaminB12.map({ .init(value: $0, unit: "mg") }),
-            vitaminC: vitaminC.map({ .init(value: $0, unit: "mg") }),
-            vitaminD: vitaminD.map({ .init(value: $0, unit: "mg") }),
-            vitaminE: vitaminE.map({ .init(value: $0, unit: "mg") }),
-            servingName: servingName,
-            servingQuantity: quantity,
-            ingredients: ingredients,
-            category: networkCategory,
-            isVerified: isVerified
-        )
+  func asNetworkFoodItem() -> FoodItem {
+    let quantity: FoodItem.Quantity?
+    if let servingValue, let servingUnitString {
+      quantity = .init(
+        value: servingValue,
+        unit: servingUnitString
+      )
+    } else {
+      quantity = nil
     }
 
-    var networkCategory: FoodItem.Category {
-        guard let dbCategory = self.category else { return .generic }
+    return FoodItem(
+      id: .init(id),
+      name: name,
+      brandName: brandName,
+      flavour: flavour,
+      country: FoodItem.Country(rawValue: rawCountry ?? ""),
+      calories: .init(value: calories, unit: "kcal"),
+      protein: .init(value: protein, unit: "g"),
+      carbohydrates: .init(value: carbohydrates, unit: "g"),
+      fat: .init(value: fat, unit: "g"),
+      saturatedFat: saturatedFat.map({ .init(value: $0, unit: "g") }),
+      transFat: transFat.map({ .init(value: $0, unit: "g") }),
+      polyunsaturatedFat: polyunsaturatedFat.map({ .init(value: $0, unit: "g") }),
+      monounsaturatedFat: monounsaturatedFat.map({ .init(value: $0, unit: "g") }),
+      fiber: fiber.map({ .init(value: $0, unit: "g") }),
+      sugar: sugar.map({ .init(value: $0, unit: "g") }),
+      cholesterol: cholesterol.map({ .init(value: $0, unit: "mg") }),
+      sodium: sodium.map({ .init(value: $0, unit: "mg") }),
+      calcium: calcium.map({ .init(value: $0, unit: "mg") }),
+      iron: iron.map({ .init(value: $0, unit: "mg") }),
+      potassium: potassium.map({ .init(value: $0, unit: "mg") }),
+      magnesium: magnesium.map({ .init(value: $0, unit: "mg") }),
+      zinc: zinc.map({ .init(value: $0, unit: "mg") }),
+      vitaminA: vitaminA.map({ .init(value: $0, unit: "mg") }),
+      vitaminB6: vitaminB6.map({ .init(value: $0, unit: "mg") }),
+      vitaminB12: vitaminB12.map({ .init(value: $0, unit: "mg") }),
+      vitaminC: vitaminC.map({ .init(value: $0, unit: "mg") }),
+      vitaminD: vitaminD.map({ .init(value: $0, unit: "mg") }),
+      vitaminE: vitaminE.map({ .init(value: $0, unit: "mg") }),
+      servingName: servingName,
+      servingQuantity: quantity,
+      ingredients: ingredients,
+      category: networkCategory,
+      isVerified: isVerified
+    )
+  }
 
-        return FoodItem.Category(rawValue: dbCategory.rawValue) ?? .generic
-    }
+  var networkCategory: FoodItem.Category {
+    guard let dbCategory = self.category else { return .generic }
+
+    return FoodItem.Category(rawValue: dbCategory.rawValue) ?? .generic
+  }
 }
--- Apps/Bloom/Supplements/UserInterface/Actions/FoodTracking/ViewModels/NutritionTrackingViewModel.swift
+++ Apps/Bloom/Supplements/UserInterface/Actions/FoodTracking/ViewModels/NutritionTrackingViewModel.swift
@@ -13,167 +13,168 @@ import TelemetryDeck
 
 @Observable @MainActor
 final class NutritionTrackingViewModel {
-    static let shared = NutritionTrackingViewModel()
+  static let shared = NutritionTrackingViewModel()
 
-    var date = Date.now
-    var suggestedMeal = FoodItemLog.Meal.breakfast
+  var date = Date.now
+  var suggestedMeal = FoodItemLog.Meal.breakfast
 
-    private let modelContext = ModelContext(ContainerHolder.shared.container)
+  private let modelContext = ModelContext(ContainerHolder.shared.container)
 
-    private init() {
-        updateMealForCurrentTime()
-    }
+  private init() {
+    updateMealForCurrentTime()
+  }
 }
 
 extension NutritionTrackingViewModel {
 
-    func updateMealForCurrentTime() {
-        date = .now
-        let hour = Calendar.current.component(.hour, from: date)
+  func updateMealForCurrentTime() {
+    date = .now
+    let hour = Calendar.current.component(.hour, from: date)
 
-        switch hour {
-        case 6 ..< 10:
-            suggestedMeal = .breakfast
-        case 11 ..< 13:
-            suggestedMeal = .lunch
-        case 17 ..< 20:
-            suggestedMeal = .dinner
-        default:
-            suggestedMeal = .snack
-        }
+    switch hour {
+    case 6 ..< 10:
+      suggestedMeal = .breakfast
+    case 11 ..< 13:
+      suggestedMeal = .lunch
+    case 17 ..< 20:
+      suggestedMeal = .dinner
+    default:
+      break
     }
+  }
 
-    /// Advances the suggested meal by one meal. If needed, the day will advance as well.
-    func advanceTimeWindow() {
-        switch suggestedMeal {
-        case .breakfast:
-            suggestedMeal = .lunch
-        case .lunch:
-            suggestedMeal = .dinner
-        case .dinner:
-            suggestedMeal = .snack
-        case .snack:
-            suggestedMeal = .breakfast
-            date = Calendar.current.date(byAdding: .day, value: 1, to: date) ?? date
-        @unknown default:
-            break
-        }
+  /// Advances the suggested meal by one meal. If needed, the day will advance as well.
+  func advanceTimeWindow() {
+    switch suggestedMeal {
+    case .breakfast:
+      suggestedMeal = .lunch
+    case .lunch:
+      suggestedMeal = .dinner
+    case .dinner:
+      suggestedMeal = .snack
+    case .snack:
+      suggestedMeal = .breakfast
+      date = Calendar.current.date(byAdding: .day, value: 1, to: date) ?? date
+    @unknown default:
+      break
     }
+  }
 
-    /// Reverses the suggested meal by one meal. If needed, the day will reverse as well.
-    func reverseTimeWindow() {
-        switch suggestedMeal {
-        case .breakfast:
-            suggestedMeal = .snack
-            date = Calendar.current.date(byAdding: .day, value: -1, to: date) ?? date
-        case .lunch:
-            suggestedMeal = .breakfast
-        case .dinner:
-            suggestedMeal = .lunch
-        case .snack:
-            suggestedMeal = .dinner
-        @unknown default:
-            break
-        }
+  /// Reverses the suggested meal by one meal. If needed, the day will reverse as well.
+  func reverseTimeWindow() {
+    switch suggestedMeal {
+    case .breakfast:
+      suggestedMeal = .snack
+      date = Calendar.current.date(byAdding: .day, value: -1, to: date) ?? date
+    case .lunch:
+      suggestedMeal = .breakfast
+    case .dinner:
+      suggestedMeal = .lunch
+    case .snack:
+      suggestedMeal = .dinner
+    @unknown default:
+      break
     }
+  }
 }
 
 extension NutritionTrackingViewModel {
 
-    func log(foodItemServings: [FoodItemServing], meal: FoodItemLog.Meal) async throws {
-        try modelContext.transaction {
-            for serving in foodItemServings {
-                let dbFoodItem: FoodItemRecord
-                if let existingFoodItem = try modelContext.fetchFoodItem(for: serving.foodItem.id.value) {
-                    dbFoodItem = existingFoodItem
-                } else {
-                    dbFoodItem = FoodItemRecord(foodItem: serving.foodItem)
-                    modelContext.insert(dbFoodItem)
-                }
-
-                let foodItemLog = FoodItemLog(
-                    id: UUID().uuidString,
-                    date: date(for: meal),
-                    meal: meal,
-                    numberOfServings: serving.serving,
-                    foodItem: dbFoodItem
-                )
-
-                modelContext.insert(foodItemLog)
-            }
+  func log(foodItemServings: [FoodItemServing], meal: FoodItemLog.Meal) async throws {
+    try modelContext.transaction {
+      for serving in foodItemServings {
+        let dbFoodItem: FoodItemRecord
+        if let existingFoodItem = try modelContext.fetchFoodItem(for: serving.foodItem.id.value) {
+          dbFoodItem = existingFoodItem
+          dbFoodItem.apply(foodItem: serving.foodItem)
+        } else {
+          dbFoodItem = FoodItemRecord(foodItem: serving.foodItem)
+          modelContext.insert(dbFoodItem)
         }
 
-        try await HealthStoreModifier.shared.updateNutrition(for: date)
-
-        TelemetryDeck.signal(
-            "Logged Food Item",
-            parameters: ["Meal" : meal.rawValue],
-            floatValue: Double(foodItemServings.count)
+        let foodItemLog = FoodItemLog(
+          id: UUID().uuidString,
+          date: date(for: meal),
+          meal: meal,
+          numberOfServings: serving.serving,
+          foodItem: dbFoodItem
         )
+
+        modelContext.insert(foodItemLog)
+      }
     }
 
-    func log(
-        foodItem: FoodItem,
-        meal: FoodItemLog.Meal,
-        numberOfServings: Double
-    ) async throws {
-        let serving = FoodItemServing(serving: numberOfServings, foodItem: foodItem)
-        try await log(foodItemServings: [serving], meal: meal)
+    try await HealthStoreModifier.shared.updateNutrition(for: date)
+
+    TelemetryDeck.signal(
+      "Logged Food Item",
+      parameters: ["Meal" : meal.rawValue],
+      floatValue: Double(foodItemServings.count)
+    )
+  }
+
+  func log(
+    foodItem: FoodItem,
+    meal: FoodItemLog.Meal,
+    numberOfServings: Double
+  ) async throws {
+    let serving = FoodItemServing(serving: numberOfServings, foodItem: foodItem)
+    try await log(foodItemServings: [serving], meal: meal)
+  }
+
+  func delete(foodItemLogs: [FoodItemLog]) async throws {
+    var dates = Set<Date>()
+
+    try? modelContext.transaction {
+      for foodItemLog in foodItemLogs {
+        dates.insert(foodItemLog.date)
+
+        try modelContext.deleteByID(foodItemLog)
+      }
     }
 
-    func delete(foodItemLogs: [FoodItemLog]) async throws {
-        var dates = Set<Date>()
+    for date in dates {
+      try await HealthStoreModifier.shared.updateNutrition(for: date)
+    }
+  }
 
-        try? modelContext.transaction {
-            for foodItemLog in foodItemLogs {
-                dates.insert(foodItemLog.date)
-                
-                try modelContext.deleteByID(foodItemLog)
-            }
-        }
-
-        for date in dates {
-            try await HealthStoreModifier.shared.updateNutrition(for: date)
-        }
+  func update(
+    foodItem: FoodItemLog,
+    numberOfServings: Double,
+    date: Date,
+    meal: FoodItemLog.Meal
+  ) async throws {
+    guard let localLog: FoodItemLog = try modelContext.existingModel(for: foodItem.persistentModelID) else {
+      throw NSError(description: "There was a problem saving the changes.")
     }
 
-    func update(
-        foodItem: FoodItemLog,
-        numberOfServings: Double,
-        date: Date,
-        meal: FoodItemLog.Meal
-    ) async throws {
-        guard let localLog: FoodItemLog = try modelContext.existingModel(for: foodItem.persistentModelID) else {
-            throw NSError(description: "There was a problem saving the changes.")
-        }
+    let oldDate = localLog.date
 
-        let oldDate = localLog.date
+    localLog.numberOfServings = numberOfServings
+    localLog.date = self.date(for: meal)
+    localLog.meal = meal
 
-        localLog.numberOfServings = numberOfServings
-        localLog.date = self.date(for: meal)
-        localLog.meal = meal
-
-        try modelContext.save()
-
-        try await HealthStoreModifier.shared.updateNutrition(for: oldDate)
-        try await HealthStoreModifier.shared.updateNutrition(for: date)
-    }
+    try modelContext.save()
+    
+    try await HealthStoreModifier.shared.updateNutrition(for: oldDate)
+    try await HealthStoreModifier.shared.updateNutrition(for: date)
+  }
 }
 
 private extension NutritionTrackingViewModel {
 
-    func date(for meal: FoodItemLog.Meal) -> Date {
-        switch meal {
-        case .breakfast:
-            Calendar.current.date(bySettingHour: 9, minute: 0, second: 0, of: date) ?? date
-        case .lunch:
-            Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: date) ?? date
-        case .dinner:
-            Calendar.current.date(bySettingHour: 17, minute: 0, second: 0, of: date) ?? date
-        case .snack:
-            Calendar.current.date(bySettingHour: 15, minute: 0, second: 0, of: date) ?? date
-        @unknown default:
-            date
-        }
+  func date(for meal: FoodItemLog.Meal) -> Date {
+    switch meal {
+    case .breakfast:
+      Calendar.current.date(bySettingHour: 9, minute: 0, second: 0, of: date) ?? date
+    case .lunch:
+      Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: date) ?? date
+    case .dinner:
+      Calendar.current.date(bySettingHour: 17, minute: 0, second: 0, of: date) ?? date
+    case .snack:
+      Calendar.current.date(bySettingHour: 15, minute: 0, second: 0, of: date) ?? date
+    @unknown default:
+      date
     }
+  }
 }
