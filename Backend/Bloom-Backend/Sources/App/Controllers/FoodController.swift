cbd612bb765446ac33a1445e33efab9d27aa2f57
Mark DiFranco <mpdifranco@icloud.com>
2024-12-17 15:46:19 -0500

    Don't throw errors when hitting OpenFoodFacts

--- Backend/Bloom-Backend/Sources/App/Controllers/FoodController.swift
+++ Backend/Bloom-Backend/Sources/App/Controllers/FoodController.swift
@@ -116,14 +116,14 @@ private extension FoodController {
     let requestBody = try request.content.decode(FoodSearchRequest.self)
 
     if let barcode = requestBody.upcCode {
-      let sections = try await searchFoodsBarcodeLocalDatabase(request, barcode: barcode)
+      let sections = await searchFoodsBarcodeLocalDatabase(request, barcode: barcode)
       if sections.isNotEmpty {
         return sections
       } else {
-        return try await searchFoodsBarcodeOpenFoodFacts(request, barcode: barcode)
+        return await searchFoodsBarcodeOpenFoodFacts(request, barcode: barcode)
       }
     } else if let name = requestBody.name {
-      return try await searchFoodsByNameLocalDatabase(
+      return await searchFoodsByNameLocalDatabase(
         request,
         name: name,
         country: requestBody.country?.country ?? .usa
@@ -133,183 +133,199 @@ private extension FoodController {
     }
   }
 
-  func searchFoodsBarcodeLocalDatabase(_ request: Request, barcode: String) async throws -> [FoodSearchResponse.Section] {
-    let foodItems = try await foodDatabaseService.searchFoods(
-      request: request,
-      barcode: barcode
-    )
+  func searchFoodsBarcodeLocalDatabase(_ request: Request, barcode: String) async -> [FoodSearchResponse.Section] {
+    do {
+      let foodItems = try await foodDatabaseService.searchFoods(
+        request: request,
+        barcode: barcode
+      )
 
-    guard foodItems.isNotEmpty else { return [] }
+      guard foodItems.isNotEmpty else { return [] }
 
-    let section = FoodSearchResponse.Section(
-      title: "Matched Barcode",
-      index: 0,
-      category: .branded,
-      foods: foodItems
-    )
+      let section = FoodSearchResponse.Section(
+        title: "Matched Barcode",
+        index: 0,
+        category: .branded,
+        foods: foodItems
+      )
 
-    return [section]
+      return [section]
+    } catch {
+      request.logger.error(error)
+    }
+    return []
   }
 
-  func searchFoodsBarcodeOpenFoodFacts(_ request: Request, barcode: String) async throws -> [FoodSearchResponse.Section] {
-    let response = try await openFoodFactsService.fetchProductImages(request, barcode: barcode)
+  func searchFoodsBarcodeOpenFoodFacts(_ request: Request, barcode: String) async -> [FoodSearchResponse.Section] {
+    do {
+      let response = try await openFoodFactsService.fetchProductImages(request, barcode: barcode)
 
-    guard let images = response.product.selectedImages else {
-      request.logger.info("Open Food Facts product had no images.")
-      return []
+      guard let images = response.product.selectedImages else {
+        request.logger.info("Open Food Facts product had no images.")
+        return []
+      }
+
+      guard
+        let packageURI = images.front?.display?.en?.uri ?? images.front?.display?.fr?.uri, // Fallback to french if there's no English
+        let nutritionLabelURI = images.nutrition?.display?.en?.uri ?? images.nutrition?.display?.fr?.uri, // Fallback to french if there's no English
+        let packaging = try await request.client.get(packageURI).body,
+        let nutritionLabel = try await request.client.get(nutritionLabelURI).body
+      else {
+        request.logger.info("Could not load images from Open Food Facts Product.")
+        return []
+      }
+
+      guard let countries = response.product.countries else {
+        request.logger.info("Could not load countries from Open Food Facts Product.")
+        return[]
+      }
+
+      let packagingData = Data(packaging.readableBytesView)
+      let nutritionLabelData = Data(nutritionLabel.readableBytesView)
+
+      let packagingMetadata = try await save(
+        image: .init(data: packagingData, fileExtension: "png") ,
+        request: request,
+        path: .foodPackaging
+      )
+      let nutritionLabelMetadata = try await save(
+        image: .init(data: nutritionLabelData, fileExtension: "png"),
+        request: request,
+        path: .nutritionLabel
+      )
+
+      let country: FoodCountry
+      if countries.contains("Canada") {
+        country = .canada
+      } else if countries.contains("United States") {
+        country = .usa
+      } else {
+        request.logger.info("[OpenFoodFacts] Unsupported country code: \(countries).")
+        return []
+      }
+
+      let aiResponse = try await openAIService.parseNewFoodItem(
+        request: request,
+        barCode: barcode,
+        country: country,
+        nutritionLabelMetadata: nutritionLabelMetadata,
+        packagingMetadata: packagingMetadata
+      )
+
+      guard let foodItemRecord = aiResponse.0 else { return [] }
+
+      foodItemRecord.source = "Open Food Facts"
+      foodItemRecord.ingredients = response.product.ingredients
+
+      guard let foodItem = foodItemRecord.asFoodItem() else { return [] }
+
+      try await foodItemRecord.save(on: request.db)
+
+      let section = FoodSearchResponse.Section(
+        title: "Matched Barcode",
+        index: 0,
+        category: .branded,
+        foods: [foodItem]
+      )
+
+      return [section]
+    } catch {
+      request.logger.error(error)
     }
-
-    guard
-      let packageURI = images.front?.display?.en?.uri ?? images.front?.display?.fr?.uri, // Fallback to french if there's no English
-      let nutritionLabelURI = images.nutrition?.display?.en?.uri ?? images.nutrition?.display?.fr?.uri, // Fallback to french if there's no English
-      let packaging = try await request.client.get(packageURI).body,
-      let nutritionLabel = try await request.client.get(nutritionLabelURI).body
-    else {
-      request.logger.info("Could not load images from Open Food Facts Product.")
-      return []
-    }
-
-    guard let countries = response.product.countries else {
-      request.logger.info("Could not load countries from Open Food Facts Product.")
-      return[]
-    }
-
-    let packagingData = Data(packaging.readableBytesView)
-    let nutritionLabelData = Data(nutritionLabel.readableBytesView)
-
-    let packagingMetadata = try await save(
-      image: .init(data: packagingData, fileExtension: "png") ,
-      request: request,
-      path: .foodPackaging
-    )
-    let nutritionLabelMetadata = try await save(
-      image: .init(data: nutritionLabelData, fileExtension: "png"),
-      request: request,
-      path: .nutritionLabel
-    )
-
-    let country: FoodCountry
-    if countries.contains("Canada") {
-      country = .canada
-    } else if countries.contains("United States") {
-      country = .usa
-    } else {
-      request.logger.info("[OpenFoodFacts] Unsupported country code: \(countries).")
-      return []
-    }
-
-    let aiResponse = try await openAIService.parseNewFoodItem(
-      request: request,
-      barCode: barcode,
-      country: country,
-      nutritionLabelMetadata: nutritionLabelMetadata,
-      packagingMetadata: packagingMetadata
-    )
-
-    guard let foodItemRecord = aiResponse.0 else { return [] }
-
-    foodItemRecord.source = "Open Food Facts"
-    foodItemRecord.ingredients = response.product.ingredients
-
-    guard let foodItem = foodItemRecord.asFoodItem() else { return [] }
-
-    try await foodItemRecord.save(on: request.db)
-
-    let section = FoodSearchResponse.Section(
-      title: "Matched Barcode",
-      index: 0,
-      category: .branded,
-      foods: [foodItem]
-    )
-
-    return [section]
+    return []
   }
 
   func searchFoodsByNameLocalDatabase(
     _ request: Request,
     name: String,
     country: FoodItemRecord.Country
-  ) async throws -> [FoodSearchResponse.Section] {
-    var sections = [FoodSearchResponse.Section]()
+  ) async -> [FoodSearchResponse.Section] {
+    do {
+      var sections = [FoodSearchResponse.Section]()
 
-    try await withThrowingTaskGroup(of: FoodSearchResponse.Section?.self) { group in
-      group.addTask {
-        let foodItems = try await foodDatabaseService.searchFoods(
-          request: request,
-          query: name,
-          category: .branded,
-          preferredCountry: country,
-          limit: 20
-        )
-        guard foodItems.isNotEmpty else { return nil }
+      try await withThrowingTaskGroup(of: FoodSearchResponse.Section?.self) { group in
+        group.addTask {
+          let foodItems = try await foodDatabaseService.searchFoods(
+            request: request,
+            query: name,
+            category: .branded,
+            preferredCountry: country,
+            limit: 20
+          )
+          guard foodItems.isNotEmpty else { return nil }
 
-        return FoodSearchResponse.Section(
-          title: "Branded",
-          index: 0,
-          category: .branded,
-          foods: foodItems
-        )
+          return FoodSearchResponse.Section(
+            title: "Branded",
+            index: 0,
+            category: .branded,
+            foods: foodItems
+          )
+        }
+        group.addTask {
+          let foodItems = try await foodDatabaseService.searchFoods(
+            request: request,
+            query: name,
+            category: .restaurant,
+            preferredCountry: country,
+            limit: 20
+          )
+          guard foodItems.isNotEmpty else { return nil }
+
+          return FoodSearchResponse.Section(
+            title: "Restaurant",
+            index: 1,
+            category: .restaurant,
+            foods: foodItems
+          )
+        }
+        group.addTask {
+          let foodItems = try await foodDatabaseService.searchFoods(
+            request: request,
+            query: name,
+            category: .fastfood,
+            preferredCountry: country,
+            limit: 20
+          )
+          guard foodItems.isNotEmpty else { return nil }
+
+          return FoodSearchResponse.Section(
+            title: "Fast Food",
+            index: 2,
+            category: .fastfood,
+            foods: foodItems
+          )
+        }
+        group.addTask {
+          let foodItems = try await foodDatabaseService.searchFoods(
+            request: request,
+            query: name,
+            category: .generic,
+            preferredCountry: country,
+            limit: 20
+          )
+          guard foodItems.isNotEmpty else { return nil }
+
+          return FoodSearchResponse.Section(
+            title: "Generic",
+            index: 3,
+            category: .generic,
+            foods: foodItems
+          )
+        }
+
+        for try await section in group {
+          guard let section else { continue }
+
+          sections.append(section)
+        }
       }
-      group.addTask {
-        let foodItems = try await foodDatabaseService.searchFoods(
-          request: request,
-          query: name,
-          category: .restaurant,
-          preferredCountry: country,
-          limit: 20
-        )
-        guard foodItems.isNotEmpty else { return nil }
 
-        return FoodSearchResponse.Section(
-          title: "Restaurant",
-          index: 1,
-          category: .restaurant,
-          foods: foodItems
-        )
-      }
-      group.addTask {
-        let foodItems = try await foodDatabaseService.searchFoods(
-          request: request,
-          query: name,
-          category: .fastfood,
-          preferredCountry: country,
-          limit: 20
-        )
-        guard foodItems.isNotEmpty else { return nil }
-
-        return FoodSearchResponse.Section(
-          title: "Fast Food",
-          index: 2,
-          category: .fastfood,
-          foods: foodItems
-        )
-      }
-      group.addTask {
-        let foodItems = try await foodDatabaseService.searchFoods(
-          request: request,
-          query: name,
-          category: .generic,
-          preferredCountry: country,
-          limit: 20
-        )
-        guard foodItems.isNotEmpty else { return nil }
-
-        return FoodSearchResponse.Section(
-          title: "Generic",
-          index: 3,
-          category: .generic,
-          foods: foodItems
-        )
-      }
-
-      for try await section in group {
-        guard let section else { continue }
-
-        sections.append(section)
-      }
+      return sections.sorted(by: { $0.index < $1.index })
+    } catch {
+      request.logger.error(error)
     }
 
-    return sections.sorted(by: { $0.index < $1.index })
+    return []
   }
 }
 
