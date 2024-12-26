1bd8d6b3db4d908313aed0fb562c192b55f710bc
Mark DiFranco <mpdifranco@icloud.com>
2024-12-26 09:01:13 -0500

    Fix error alert for AI scan

--- Apps/Bloom/Supplements/UserInterface/Actions/FoodTracking/AIFoodScanner/AIFoodScannerView.swift
+++ Apps/Bloom/Supplements/UserInterface/Actions/FoodTracking/AIFoodScanner/AIFoodScannerView.swift
@@ -12,297 +12,296 @@ import AVFoundation
 
 struct AIFoodScannerView: View {
 
-    init() {
-        self.cameraManager = CameraManager.create(with: captureSession)
+  init() {
+    self.cameraManager = CameraManager.create(with: captureSession)
+  }
+
+  @State private var viewModel = ViewModel()
+
+  @State private var startScanToggle = false
+  @State private var scanResultsToggle = false
+  @State private var saveComplete = false
+  @State private var errorToggle = false
+  @State private var alertDetails: AlertDetails?
+  @FocusState private var focusedIndex: Int?
+
+  @StateObject var permissionManager = CameraPermissionManager.shared
+
+  @Environment(\.dismiss) private var dismiss
+
+  private let nutritionViewModel = NutritionTrackingViewModel.shared
+  private let cameraManager: CameraManager
+  private let captureSession = AVCaptureSession()
+
+  var body: some View {
+    GeometryReader { proxy in
+      VStack(spacing: 0) {
+        scanAreaView
+          .frame(height: proxy.size.height * imageScanAspect)
+          .clipped()
+
+        scannedItemsView
+          .background {
+            RoundedRectangle(cornerRadius: 30)
+              .fill(.background)
+              .ignoresSafeArea(edges: .bottom)
+          }
+          .padding(.top, -30)
+      }
     }
-
-    @State private var viewModel = ViewModel()
-
-    @State private var startScanToggle = false
-    @State private var scanResultsToggle = false
-    @State private var saveComplete = false
-    @State private var errorToggle = false
-    @State private var error: Error?
-    @State private var alertDetails: AlertDetails?
-    @FocusState private var focusedIndex: Int?
-
-    @StateObject var permissionManager = CameraPermissionManager.shared
-
-    @Environment(\.dismiss) private var dismiss
-
-    private let nutritionViewModel = NutritionTrackingViewModel.shared
-    private let cameraManager: CameraManager
-    private let captureSession = AVCaptureSession()
-
-    var body: some View {
-        GeometryReader { proxy in
-            VStack(spacing: 0) {
-                scanAreaView
-                    .frame(height: proxy.size.height * imageScanAspect)
-                    .clipped()
-
-                scannedItemsView
-                    .background {
-                        RoundedRectangle(cornerRadius: 30)
-                            .fill(.background)
-                            .ignoresSafeArea(edges: .bottom)
-                    }
-                    .padding(.top, -30)
-            }
+    .sensoryFeedback(.error, trigger: errorToggle)
+    .sensoryFeedback(.impact, trigger: startScanToggle)
+    .sensoryFeedback(.success, trigger: scanResultsToggle)
+    .sensoryFeedback(.success, trigger: saveComplete)
+    .ignoresSafeArea(edges: .top)
+    .presentationCompactAdaptation(.fullScreenCover)
+    .animation(.bouncy, value: viewModel.image)
+    .animation(.default, value: viewModel.isLoading)
+    .tint(.mutedGreen)
+    .onAppear {
+      Task {
+        await permissionManager.checkPermission()
+        if permissionManager.permissionState == .granted {
+          await cameraManager.start()
         }
-        .sensoryFeedback(.error, trigger: errorToggle)
-        .sensoryFeedback(.impact, trigger: startScanToggle)
-        .sensoryFeedback(.success, trigger: scanResultsToggle)
-        .sensoryFeedback(.success, trigger: saveComplete)
-        .ignoresSafeArea(edges: .top)
-        .presentationCompactAdaptation(.fullScreenCover)
-        .animation(.bouncy, value: viewModel.image)
-        .animation(.default, value: viewModel.isLoading)
-        .tint(.mutedGreen)
+      }
+    }
+    .onDisappear {
+      Task {
+        await cameraManager.stop()
+      }
+    }
+    .alert(alertDetails: $alertDetails)
+    .alert(error: $viewModel.error)
+    .onChange(of: viewModel.error as? NSError) { oldValue, newValue in
+      guard newValue != nil else { return }
+
+      errorToggle.toggle()
+    }
+  }
+}
+
+private extension AIFoodScannerView {
+
+  var imageScanAspect: CGFloat {
+    if viewModel.image == nil {
+      return 0.6
+    }
+    return 0.4
+  }
+
+  @ViewBuilder
+  var scanAreaView: some View {
+    switch permissionManager.permissionState {
+    case .granted:
+      cameraView
+    case .denied:
+      CameraPermissionDeniedView()
         .onAppear {
-            Task {
-                await permissionManager.checkPermission()
-                if permissionManager.permissionState == .granted {
-                    await cameraManager.start()
-                }
-            }
+          alertDetails = permissionManager.permissionAlert
         }
-        .onDisappear {
-            Task {
-                await cameraManager.stop()
-            }
-        }
-        .alert(alertDetails: $alertDetails)
-        .alert(error: $error)
-        .onChange(of: error as? NSError) { oldValue, newValue in
-            guard newValue != nil else { return }
-
-            errorToggle.toggle()
-        }
+    case .pending:
+      Rectangle()
+        .fill(.black)
+        .ignoresSafeArea()
+        .aspectRatio(contentMode: .fit)
     }
-}
+  }
 
-private extension AIFoodScannerView {
-
-    var imageScanAspect: CGFloat {
-        if viewModel.image == nil {
-            return 0.6
+  var cameraView: some View {
+    ZStack {
+      CameraPreview(
+        session: captureSession,
+        gravity: .resizeAspectFill
+      ) { focusPoint in
+        Task {
+          await cameraManager.setFocus(for: focusPoint)
         }
-        return 0.4
+      }
+
+      if let image = viewModel.image {
+        Image(uiImage: image)
+          .resizable()
+          .aspectRatio(contentMode: .fill)
+      }
     }
+  }
 
-    @ViewBuilder
-    var scanAreaView: some View {
-        switch permissionManager.permissionState {
-        case .granted:
-          cameraView
-        case .denied:
-          CameraPermissionDeniedView()
-            .onAppear {
-              alertDetails = permissionManager.permissionAlert
-            }
-        case .pending:
-          Rectangle()
-            .fill(.black)
-            .ignoresSafeArea()
-            .aspectRatio(contentMode: .fit)
-        }
-    }
-
-    var cameraView: some View {
-        ZStack {
-            CameraPreview(
-                session: captureSession,
-                gravity: .resizeAspectFill
-            ) { focusPoint in
-                Task {
-                    await cameraManager.setFocus(for: focusPoint)
-                }
-            }
-
-            if let image = viewModel.image {
-                Image(uiImage: image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-            }
-        }
-    }
-
-    var scannedItemsView: some View {
+  var scannedItemsView: some View {
+    VStack {
+      if viewModel.isLoading {
         VStack {
-            if viewModel.isLoading {
-                VStack {
-                    Spacer()
-                    CircularSpinnerView()
-                        .foregroundStyle(.tint)
-                    Text("Analyzing...")
-                        .font(.title2)
-                        .bold()
-                        .fontDesign(.rounded)
-                    Spacer()
-                }
-                .horizontallyCentered()
-            } else if !viewModel.hasScannedAtLeastOnce {
-                Spacer()
-                ContentUnavailableView("Scan Food", systemImage: "fork.knife")
-                Spacer()
-            } else {
-                VStack(spacing: 0) {
-                    if viewModel.servings.isEmpty {
-                        Spacer()
-                        ContentUnavailableView("No Food Identified", systemImage: "fork.knife")
-                        Spacer()
-                    } else {
-                        foodResultsHeader
-
-                        ScrollView {
-                            VStack {
-                                SectionTitleView("Identified Food")
-                                    .padding(.horizontal)
-                                ForEachEnumerated(viewModel.servings) { (index, serving) in
-                                    if viewModel.servings.count > index { // Fixes dumb bug where viewModel.servings is empty but we try and load a cell.
-                                        AIScanFoodItemCell(foodItemServing: $viewModel.servings[index])
-                                            .focused($focusedIndex, equals: index)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                .padding(.horizontal)
-            }
-
-            HStack(spacing: 6) {
-                if focusedIndex != nil {
-                    textEditorBottomBar
-                } else {
-                    logFoodBottomBar
-                }
-            }
-            .padding()
+          Spacer()
+          CircularSpinnerView()
+            .foregroundStyle(.tint)
+          Text("Analyzing...")
+            .font(.title2)
+            .bold()
+            .fontDesign(.rounded)
+          Spacer()
         }
+        .horizontallyCentered()
+      } else if !viewModel.hasScannedAtLeastOnce {
+        Spacer()
+        ContentUnavailableView("Scan Food", systemImage: "fork.knife")
+        Spacer()
+      } else {
+        VStack(spacing: 0) {
+          if viewModel.servings.isEmpty {
+            Spacer()
+            ContentUnavailableView("No Food Identified", systemImage: "fork.knife")
+            Spacer()
+          } else {
+            foodResultsHeader
 
-    }
-}
-
-private extension AIFoodScannerView {
-
-    @ViewBuilder
-    var foodResultsHeader: some View {
-        FoodItemLogPickerHeader()
-            .horizontallyCentered()
-            .padding(.vertical, 4)
-            .background {
-                Button {
-                    viewModel.reset()
-                } label: {
-                    Image(systemName: "arrow.counterclockwise")
-                        .bold()
-                }
-                .horizontalAlignment(.trailing)
-            }
-        Divider()
-    }
-
-    @ViewBuilder
-    var logFoodBottomBar: some View {
-        Button {
-            dismiss()
-        } label: {
-            Image(systemName: "xmark")
-                .bold()
-                .frame(square: 55)
-                .background {
-                    RoundedRectangle(cornerRadius: 16)
-                        .fill(.tint)
-                }
-                .foregroundStyle(.white)
-        }
-
-        if viewModel.servings.isNotEmpty {
-            Button {
-                Task {
-                    do {
-                        try await save()
-                        dismiss()
-                    } catch {
-                        self.error = error
-                    }
-                }
-            } label: {
-                Text("Log All")
-                    .bold()
-                    .horizontallyCentered()
-                    .frame(height: 55)
-                    .background {
-                        RoundedRectangle(cornerRadius: 16)
-                            .fill(.tint)
-                    }
-                    .foregroundStyle(.white)
-            }
-        } else {
-            Button {
-                startScanToggle.toggle()
-                Task {
-                  guard permissionManager.permissionState == .granted else {
-                    alertDetails = permissionManager.permissionAlert
-                    return
+            ScrollView {
+              VStack {
+                SectionTitleView("Identified Food")
+                  .padding(.horizontal)
+                ForEachEnumerated(viewModel.servings) { (index, serving) in
+                  if viewModel.servings.count > index { // Fixes dumb bug where viewModel.servings is empty but we try and load a cell.
+                    AIScanFoodItemCell(foodItemServing: $viewModel.servings[index])
+                      .focused($focusedIndex, equals: index)
                   }
-                    guard let image = await cameraManager.capture() else { return } // TODO: Throw error?
-
-                    viewModel.image = image
-                    await viewModel.performAIFoodLog(for: image)
-
-                    if viewModel.servings.isNotEmpty {
-                        scanResultsToggle.toggle()
-                    }
                 }
-            } label: {
-                Text("Scan")
-                    .bold()
-                    .horizontallyCentered()
-                    .frame(height: 55)
-                    .background {
-                        RoundedRectangle(cornerRadius: 16)
-                            .fill(.tint)
-                    }
-                    .foregroundStyle(.white)
+              }
             }
-            .disabled(viewModel.isLoading)
+          }
         }
+        .padding(.horizontal)
+      }
+
+      HStack(spacing: 6) {
+        if focusedIndex != nil {
+          textEditorBottomBar
+        } else {
+          logFoodBottomBar
+        }
+      }
+      .padding()
     }
 
-    var textEditorBottomBar: some View {
-        Button {
-            focusedIndex = nil
-        } label: {
-            Text("Done")
-                .bold()
-                .horizontallyCentered()
-                .frame(height: 55)
-                .background {
-                    RoundedRectangle(cornerRadius: 16)
-                        .fill(.tint)
-                }
-                .foregroundStyle(.white)
-        }
-    }
+  }
 }
 
 private extension AIFoodScannerView {
 
-    func save() async throws {
-        let meal = nutritionViewModel.suggestedMeal
-        try await nutritionViewModel.log(
-            foodItemServings: viewModel.servings,
-            meal: meal
-        )
+  @ViewBuilder
+  var foodResultsHeader: some View {
+    FoodItemLogPickerHeader()
+      .horizontallyCentered()
+      .padding(.vertical, 4)
+      .background {
+        Button {
+          viewModel.reset()
+        } label: {
+          Image(systemName: "arrow.counterclockwise")
+            .bold()
+        }
+        .horizontalAlignment(.trailing)
+      }
+    Divider()
+  }
 
-        saveComplete.toggle()
-        SoundPlayer.playLogHealthData()
+  @ViewBuilder
+  var logFoodBottomBar: some View {
+    Button {
+      dismiss()
+    } label: {
+      Image(systemName: "xmark")
+        .bold()
+        .frame(square: 55)
+        .background {
+          RoundedRectangle(cornerRadius: 16)
+            .fill(.tint)
+        }
+        .foregroundStyle(.white)
     }
+
+    if viewModel.servings.isNotEmpty {
+      Button {
+        Task {
+          do {
+            try await save()
+            dismiss()
+          } catch {
+            self.viewModel.error = error
+          }
+        }
+      } label: {
+        Text("Log")
+          .bold()
+          .horizontallyCentered()
+          .frame(height: 55)
+          .background {
+            RoundedRectangle(cornerRadius: 16)
+              .fill(.tint)
+          }
+          .foregroundStyle(.white)
+      }
+    } else {
+      Button {
+        startScanToggle.toggle()
+        Task {
+          guard permissionManager.permissionState == .granted else {
+            alertDetails = permissionManager.permissionAlert
+            return
+          }
+          guard let image = await cameraManager.capture() else { return } // TODO: Throw error?
+
+          viewModel.image = image
+          await viewModel.performAIFoodLog(for: image)
+
+          if viewModel.servings.isNotEmpty {
+            scanResultsToggle.toggle()
+          }
+        }
+      } label: {
+        Text("Scan")
+          .bold()
+          .horizontallyCentered()
+          .frame(height: 55)
+          .background {
+            RoundedRectangle(cornerRadius: 16)
+              .fill(.tint)
+          }
+          .foregroundStyle(.white)
+      }
+      .disabled(viewModel.isLoading)
+    }
+  }
+
+  var textEditorBottomBar: some View {
+    Button {
+      focusedIndex = nil
+    } label: {
+      Text("Done")
+        .bold()
+        .horizontallyCentered()
+        .frame(height: 55)
+        .background {
+          RoundedRectangle(cornerRadius: 16)
+            .fill(.tint)
+        }
+        .foregroundStyle(.white)
+    }
+  }
+}
+
+private extension AIFoodScannerView {
+
+  func save() async throws {
+    let meal = nutritionViewModel.suggestedMeal
+    try await nutritionViewModel.log(
+      foodItemServings: viewModel.servings,
+      meal: meal
+    )
+
+    saveComplete.toggle()
+    SoundPlayer.playLogHealthData()
+  }
 }
 
 #Preview {
-    AIFoodScannerView()
+  AIFoodScannerView()
 }
--- Apps/Bloom/Supplements/UserInterface/Actions/FoodTracking/AIFoodScanner/AIFoodScannerViewModel.swift
+++ Apps/Bloom/Supplements/UserInterface/Actions/FoodTracking/AIFoodScanner/AIFoodScannerViewModel.swift
@@ -11,44 +11,45 @@ import DataContainer
 import TelemetryDeck
 
 extension AIFoodScannerView {
-    @Observable @MainActor
-    final class ViewModel {
-        var image: UIImage?
-        var hasScannedAtLeastOnce = false
-        var isLoading = false
-        var servings = [FoodItemServing]()
-        var error: Error?
-    }
+  @Observable @MainActor
+  final class ViewModel {
+    var image: UIImage?
+    var hasScannedAtLeastOnce = false
+    var isLoading = false
+    var servings = [FoodItemServing]()
+    var error: Error?
+  }
 }
 
 extension AIFoodScannerView.ViewModel {
 
-    func performAIFoodLog(for image: UIImage) async {
-        guard let smallerImage = image.resized(toWidth: 800) else { return }
+  func performAIFoodLog(for image: UIImage) async {
+    guard let smallerImage = image.resized(toWidth: 800) else { return }
 
-        do {
-            hasScannedAtLeastOnce = true
-            isLoading = true
-            let response = try await NetworkRequester.shared.foodAIEstimate(image: smallerImage)
+    do {
+      hasScannedAtLeastOnce = true
+      isLoading = true
+      let response = try await NetworkRequester.shared.foodAIEstimate(image: smallerImage)
 
-            self.servings = response.servings.map({ $0.asServing() })
-            isLoading = false
+      self.servings = response.servings.map({ $0.asServing() })
+      isLoading = false
 
-            if servings.isEmpty {
-                self.image = nil
-            } else {
-                TelemetryDeck.signal("AI Food Scan", floatValue: Double(servings.count))
-            }
-        } catch {
-            self.error = error
-        }
+      if servings.isEmpty {
+        self.image = nil
+      } else {
+        TelemetryDeck.signal("AI Food Scan", floatValue: Double(servings.count))
+      }
+    } catch {
+      self.error = error
+      isLoading = false
     }
+  }
 
-    func reset() {
-        hasScannedAtLeastOnce = false
-        image = nil
-        isLoading = false
-        error = nil
-        servings.removeAll()
-    }
+  func reset() {
+    hasScannedAtLeastOnce = false
+    image = nil
+    isLoading = false
+    error = nil
+    servings.removeAll()
+  }
 }
