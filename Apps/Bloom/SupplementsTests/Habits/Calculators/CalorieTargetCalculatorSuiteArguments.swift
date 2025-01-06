4fab824b3aa509ebd6a4a4b8959d8f82428275c2
Mark DiFranco <mpdifranco@icloud.com>
2025-01-06 15:54:24 -0500

    Add another test case

--- Apps/Bloom/SupplementsTests/Habits/Calculators/CalorieTargetCalculatorSuiteArguments.swift
+++ Apps/Bloom/SupplementsTests/Habits/Calculators/CalorieTargetCalculatorSuiteArguments.swift
@@ -13,162 +13,171 @@ import BloomFoundation
 
 extension CalorieTargetCalculatorTestSuite {
 
-    static var noExistinHabitsArguments: [(Input, Double)] {
-        [
-            (Input(
-                "Sedentary lose weight slowly",
-                dietary: 2080,
-                bodyMass: 160,
-                targetWeight: 140,
-                activityLevel: .sedentary,
-                goal: .loseWeight,
-                speed: .slow
-            ), 1900),
-            (Input(
-                "Light lose weight slowly",
-                dietary: 2400,
-                bodyMass: 160,
-                targetWeight: 140,
-                activityLevel: .light,
-                goal: .loseWeight,
-                speed: .slow
-            ), 2200),
-            (Input(
-                "High lose weight slowly",
-                dietary: 2720,
-                bodyMass: 160,
-                targetWeight: 140,
-                activityLevel: .high,
-                goal: .loseWeight,
-                speed: .slow
-            ), 2600),
-            (Input(
-                "Sedentary lose weight moderately",
-                dietary: 2080,
-                bodyMass: 160,
-                targetWeight: 140,
-                activityLevel: .sedentary,
-                goal: .loseWeight,
-                speed: .moderate
-            ), 1800),
-            (Input(
-                "Light lose weight moderately",
-                dietary: 2400,
-                bodyMass: 160,
-                targetWeight: 140,
-                activityLevel: .light,
-                goal: .loseWeight,
-                speed: .moderate
-            ), 2080),
-            (Input(
-                "High lose weight moderatly",
-                dietary: 2600,
-                bodyMass: 160,
-                targetWeight: 140,
-                activityLevel: .high,
-                goal: .loseWeight,
-                speed: .moderate
-            ), 2400),
-            (Input(
-                "Sedentary lose weight slowly 220 lbs",
-                dietary: 2860,
-                bodyMass: 220,
-                targetWeight: 140,
-                activityLevel: .sedentary,
-                goal: .loseWeight,
-                speed: .slow
-            ), 2600),
-            (Input(
-                "Sedentary maintain weight 220 lbs",
-                dietary: 2860,
-                bodyMass: 220,
-                targetWeight: 220,
-                activityLevel: .sedentary,
-                goal: .maintainWeight,
-                speed: .slow
-            ), 2900),
-            (Input(
-                "Sedentary gain weight 220 lbs",
-                dietary: 2860,
-                bodyMass: 220,
-                targetWeight: 240,
-                activityLevel: .sedentary,
-                goal: .gainWeight,
-                speed: .slow
-            ), 3700),
-            (Input(
-                "Dietary lose weight slow",
-                dietary: 2000,
-                bodyMass: 160,
-                targetWeight: 140,
-                activityLevel: .sedentary,
-                goal: .loseWeight,
-                speed: .slow
-            ), 1900),
-            (Input(
-                "Dietary lose weight moderate",
-                dietary: 2000,
-                bodyMass: 160,
-                targetWeight: 140,
-                activityLevel: .sedentary,
-                goal: .loseWeight,
-                speed: .moderate
-            ), 1800),
-            (Input(
-                "Dietary lose weight fast",
-                dietary: 2000,
-                bodyMass: 160,
-                targetWeight: 140,
-                activityLevel: .sedentary,
-                goal: .loseWeight,
-                speed: .fast
-            ), 1600),
-            (Input(
-                "Lose weight high activity existing habit",
-                dietary: 2000,
-                bodyMass: 180,
-                targetWeight: 140,
-                activityLevel: .high,
-                goal: .loseWeight,
-                speed: .moderate,
-                existingHabit: HabitDTO(
-                    id: nil,
-                    targetMetric: .calories,
-                    value: 1650,
-                    unitString: HKUnit.largeCalorie().unitString,
-                    startDate: .now,
-                    endDate: nil,
-                    lastNotificationDate: nil,
-                    isSuggested: true,
-                    isUserEdited: false,
-                    vitalKind: .nutrition,
-                    context: ""
-                )
-            ),
-            1650),
-            (Input(
-                "Maintain weight high activity existing habit",
-                dietary: 2000,
-                bodyMass: 180,
-                targetWeight: 140,
-                activityLevel: .high,
-                goal: .maintainWeight,
-                speed: .moderate,
-                existingHabit: HabitDTO(
-                    id: nil,
-                    targetMetric: .calories,
-                    value: 1650,
-                    unitString: HKUnit.largeCalorie().unitString,
-                    startDate: .now,
-                    endDate: nil,
-                    lastNotificationDate: nil,
-                    isSuggested: true,
-                    isUserEdited: false,
-                    vitalKind: .nutrition,
-                    context: ""
-                )
-            ),
-            3100),
-        ]
-    }
+  static var noExistinHabitsArguments: [(Input, Double)] {
+    [
+      (Input(
+        "Light Activity lose weight moderately",
+        dietary: 1700,
+        bodyMass: 167,
+        targetWeight: 145,
+        activityLevel: .light,
+        goal: .loseWeight,
+        speed: .moderate
+      ), 2200), // TODO: Kaitlyn this seems wrong
+      (Input(
+        "Sedentary lose weight slowly",
+        dietary: 2080,
+        bodyMass: 160,
+        targetWeight: 140,
+        activityLevel: .sedentary,
+        goal: .loseWeight,
+        speed: .slow
+      ), 1900),
+      (Input(
+        "Light lose weight slowly",
+        dietary: 2400,
+        bodyMass: 160,
+        targetWeight: 140,
+        activityLevel: .light,
+        goal: .loseWeight,
+        speed: .slow
+      ), 2200),
+      (Input(
+        "High lose weight slowly",
+        dietary: 2720,
+        bodyMass: 160,
+        targetWeight: 140,
+        activityLevel: .high,
+        goal: .loseWeight,
+        speed: .slow
+      ), 2600),
+      (Input(
+        "Sedentary lose weight moderately",
+        dietary: 2080,
+        bodyMass: 160,
+        targetWeight: 140,
+        activityLevel: .sedentary,
+        goal: .loseWeight,
+        speed: .moderate
+      ), 1800),
+      (Input(
+        "Light lose weight moderately",
+        dietary: 2400,
+        bodyMass: 160,
+        targetWeight: 140,
+        activityLevel: .light,
+        goal: .loseWeight,
+        speed: .moderate
+      ), 2080),
+      (Input(
+        "High lose weight moderatly",
+        dietary: 2600,
+        bodyMass: 160,
+        targetWeight: 140,
+        activityLevel: .high,
+        goal: .loseWeight,
+        speed: .moderate
+      ), 2400),
+      (Input(
+        "Sedentary lose weight slowly 220 lbs",
+        dietary: 2860,
+        bodyMass: 220,
+        targetWeight: 140,
+        activityLevel: .sedentary,
+        goal: .loseWeight,
+        speed: .slow
+      ), 2600),
+      (Input(
+        "Sedentary maintain weight 220 lbs",
+        dietary: 2860,
+        bodyMass: 220,
+        targetWeight: 220,
+        activityLevel: .sedentary,
+        goal: .maintainWeight,
+        speed: .slow
+      ), 2900),
+      (Input(
+        "Sedentary gain weight 220 lbs",
+        dietary: 2860,
+        bodyMass: 220,
+        targetWeight: 240,
+        activityLevel: .sedentary,
+        goal: .gainWeight,
+        speed: .slow
+      ), 3700),
+      (Input(
+        "Dietary lose weight slow",
+        dietary: 2000,
+        bodyMass: 160,
+        targetWeight: 140,
+        activityLevel: .sedentary,
+        goal: .loseWeight,
+        speed: .slow
+      ), 1900),
+      (Input(
+        "Dietary lose weight moderate",
+        dietary: 2000,
+        bodyMass: 160,
+        targetWeight: 140,
+        activityLevel: .sedentary,
+        goal: .loseWeight,
+        speed: .moderate
+      ), 1800),
+      (Input(
+        "Dietary lose weight fast",
+        dietary: 2000,
+        bodyMass: 160,
+        targetWeight: 140,
+        activityLevel: .sedentary,
+        goal: .loseWeight,
+        speed: .fast
+      ), 1600),
+      (Input(
+        "Lose weight high activity existing habit",
+        dietary: 2000,
+        bodyMass: 180,
+        targetWeight: 140,
+        activityLevel: .high,
+        goal: .loseWeight,
+        speed: .moderate,
+        existingHabit: HabitDTO(
+          id: nil,
+          targetMetric: .calories,
+          value: 1650,
+          unitString: HKUnit.largeCalorie().unitString,
+          startDate: .now,
+          endDate: nil,
+          lastNotificationDate: nil,
+          isSuggested: true,
+          isUserEdited: false,
+          vitalKind: .nutrition,
+          context: ""
+        )
+      ),
+       1650),
+      (Input(
+        "Maintain weight high activity existing habit",
+        dietary: 2000,
+        bodyMass: 180,
+        targetWeight: 140,
+        activityLevel: .high,
+        goal: .maintainWeight,
+        speed: .moderate,
+        existingHabit: HabitDTO(
+          id: nil,
+          targetMetric: .calories,
+          value: 1650,
+          unitString: HKUnit.largeCalorie().unitString,
+          startDate: .now,
+          endDate: nil,
+          lastNotificationDate: nil,
+          isSuggested: true,
+          isUserEdited: false,
+          vitalKind: .nutrition,
+          context: ""
+        )
+      ),
+       3100),
+    ]
+  }
 }
