f802f01a3b56391a8dac062439286674319c34ad
Mark DiFranco <mpdifranco@icloud.com>
2025-01-21 21:05:09 -0500

    Remove Why we charge FAQ

--- Apps/Bloom/Bloom/UserInterface/Paywall/Components/BloomPlusFAQView.swift
+++ Apps/Bloom/Bloom/UserInterface/Paywall/Components/BloomPlusFAQView.swift
@@ -25,10 +25,10 @@ struct BloomPlusFAQView: View {
           question: "Can I track food in this app?",
           answer: "Yes! You can use Bloom to scan labels or even take photos of your meals."
         )
-        BloomPlusFAQCell(
-          question: "Why do you charge for Bloom?",
-          answer: "We charge for Bloom in order to make sure you always have the best health recommendations. We're always improving the app and making sure your health is our top priority. We also have operational costs to keep the app running."
-        )
+//        BloomPlusFAQCell(
+//          question: "Why do you charge for Bloom?",
+//          answer: "We charge for Bloom in order to make sure you always have the best health recommendations. We're always improving the app and making sure your health is our top priority. We also have operational costs to keep the app running."
+//        )
         BloomPlusFAQCell(
           question: "How is my health data secured?",
           answer: "Privacy is one of our core values. Your health data never leaves your device, so there's no risk of it getting leaked or accessed by a third party. We explicitly don't track food you search for, and your food logs only exist on your device."
