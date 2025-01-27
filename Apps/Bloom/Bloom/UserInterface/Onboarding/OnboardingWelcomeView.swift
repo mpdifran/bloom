71b39200fd0a885a8570100ab557fb08c2153107
Mark DiFranco <mpdifranco@icloud.com>
2025-01-27 11:27:33 -0500

    Continue when autofilling name in onboarding

--- Apps/Bloom/Bloom/UserInterface/Onboarding/OnboardingWelcomeView.swift
+++ Apps/Bloom/Bloom/UserInterface/Onboarding/OnboardingWelcomeView.swift
@@ -10,94 +10,107 @@ import AppUI
 import TelemetryDeck
 
 struct OnboardingWelcomeView: View {
-    var onContinue: () -> Void
+  var onContinue: () -> Void
 
-    @State private var index = 1
-    @State private var didContinue = false
+  @State private var index = 1
+  @State private var didContinue = false
 
-    @FocusState private var isFocused: Bool
+  @FocusState private var isFocused: Bool
 
-    @ObservedObject private var healthManager = HealthManager.shared
+  @ObservedObject private var healthManager = HealthManager.shared
 
-    var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 20) {
-                Image(.bloomAppIcon)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100)
+  var body: some View {
+    ScrollView {
+      VStack(alignment: .leading, spacing: 20) {
+        Image(.bloomAppIcon)
+          .resizable()
+          .scaledToFit()
+          .frame(width: 100)
 
-                Text("Hey there!")
-                    .appear(with: 1, currentIndex: index)
+        Text("Hey there!")
+          .appear(with: 1, currentIndex: index)
 
-                Text("I'm Bloom, your new personal Health Assistant.")
-                    .transition(.opacity)
-                    .appear(with: 2, currentIndex: index)
+        Text("I'm Bloom, your new personal Health Assistant.")
+          .transition(.opacity)
+          .appear(with: 2, currentIndex: index)
 
-                Group {
-                    Text("What's your first name?")
+        Group {
+          Text("What's your first name?")
 
-                    TextField("", text: $healthManager.name, prompt: Text("Your Name"))
-                        .textContentType(.givenName)
-                        .cardContainer(fill: .background.secondary)
-                        .focused($isFocused)
-                        .submitLabel(.continue)
-                        .disabled(index != 3)
-                        .onSubmit {
-                            withAnimation {
-                                index += 1
-                            }
-                        }
-                        .onAppear {
-                            isFocused = true
-                        }
-                }
-                .transition(.blurReplace)
-                .appear(with: 3, currentIndex: index)
-
-                Text("Nice to meet you, \(healthManager.name)! Let's get to know each other a bit...")
-                    .transition(.opacity)
-                    .appear(with: 4, currentIndex: index)
-
-                Spacer()
+          TextField("", text: $healthManager.name, prompt: Text("Your Name"))
+            .textContentType(.givenName)
+            .cardContainer(fill: .background.secondary)
+            .focused($isFocused)
+            .submitLabel(.done)
+            .disabled(index != 3)
+            .onSubmit {
+              didSubmitName()
             }
-            .padding()
-            .horizontalAlignment(.leading)
-        }
-        .onboardingTextStyle()
-        .topSafeAreaBlur()
-        .sensoryFeedback(.selection, trigger: index)
-        .sensoryFeedback(.selection, trigger: didContinue)
-        .animation(.default, value: index)
-        .if(index >= 4) {
-            $0.shelf {
-                Button("Sounds great!") {
-                    didContinue.toggle()
-                    onContinue()
-                }
-                .buttonStyle(.onboarding)
+            .onAppear {
+              isFocused = true
+            }
+            .onChange(of: healthManager.name) { oldValue, newValue in
+              if
+                newValue.isNotEmpty &&
+                newValue.count >= 2 &&
+                oldValue.isEmpty
+              {
+                didSubmitName()
+              }
             }
         }
-        .task {
-            while index < 3 {
-                await advanceIndex()
-            }
-        }
-        .onAppear {
-            TelemetryDeck.signal("OB Welcome")
-        }
+        .transition(.blurReplace)
+        .appear(with: 3, currentIndex: index)
+
+        Text("Nice to meet you, \(healthManager.name)! Let's get to know each other a bit...")
+          .transition(.opacity)
+          .appear(with: 4, currentIndex: index)
+
+        Spacer()
+      }
+      .padding()
+      .horizontalAlignment(.leading)
     }
+    .onboardingTextStyle()
+    .topSafeAreaBlur()
+    .sensoryFeedback(.selection, trigger: index)
+    .sensoryFeedback(.selection, trigger: didContinue)
+    .animation(.default, value: index)
+    .if(index >= 4) {
+      $0.shelf {
+        Button("Sounds great!") {
+          didContinue.toggle()
+          onContinue()
+        }
+        .buttonStyle(.onboarding)
+      }
+    }
+    .task {
+      while index < 3 {
+        await advanceIndex()
+      }
+    }
+    .onAppear {
+      TelemetryDeck.signal("OB Welcome")
+    }
+  }
 }
 
 private extension OnboardingWelcomeView {
 
-    func advanceIndex() async {
-        await Delay(1700)
-
-        index += 1
+  func didSubmitName() {
+    withAnimation {
+      index += 1
     }
+  }
+
+  func advanceIndex() async {
+    await Delay(1700)
+
+    index += 1
+  }
 }
 
 #Preview {
-    OnboardingWelcomeView { }
+  OnboardingWelcomeView { }
 }
