6188d153931b709030906484fd547efcc8719614
Zach Radford <zach.radford@faire.com>
2025-01-27 20:57:09 -0500

    map frequently logged

--- Apps/Bloom/DataContainer/SwiftData/ModelActors/FoodItemLogModelActor.swift
+++ Apps/Bloom/DataContainer/SwiftData/ModelActors/FoodItemLogModelActor.swift
@@ -70,4 +70,38 @@ public extension FoodItemLogModelActor {
         descriptor.fetchLimit = limit
         return try context.fetch(descriptor).map { $0.asDTO() }
     }
+
+  func fetchFrequentLogs(for meal: FoodItemLog.Meal, limit: Int? = nil) throws -> [FoodItemLogDTO] {
+    // Calculate 2 months ago.
+    guard let twoMonthsAgo = Calendar.current.date(byAdding: .month, value: -2, to: Date()) else {
+      return []
+    }
+
+    let descriptor = FetchDescriptor<FoodItemLog>(
+      predicate: #Predicate<FoodItemLog> { model in
+        model.date >= twoMonthsAgo
+        // TODO: Zach - we cannot query enums stored in SwiftData. Their rawValue must be on the model.
+        // https://forums.developer.apple.com/forums/thread/735638
+        // Do this in the V6 migration.
+        // && model.mealRawValue == meal.rawValue
+      }
+    )
+
+    let logs = try context.fetch(descriptor)
+
+    // Count occurrences of each food item, sort by most frequent.
+    var frequencyMap = Dictionary(grouping: logs, by: { $0.foodItem?.id })
+      .compactMapValues { $0.count }
+      .sorted { $0.value > $1.value }
+
+    // If a limit is set, apply that to the mapping.
+    if let limit {
+      frequencyMap = Array(frequencyMap.prefix(limit))
+    }
+
+    // Map food logs in sorted order based on frequency.
+    return frequencyMap.compactMap { foodItemID, _ in
+      logs.first { $0.foodItem?.id == foodItemID }?.asDTO()
+    }
+  }
 }
