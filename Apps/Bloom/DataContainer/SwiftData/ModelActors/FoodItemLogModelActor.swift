e126069f3bc4445f06ed9a87b1079c390efd2c13
Zach Radford <zach.radford@faire.com>
2025-01-30 20:19:27 -0500

    address PR comments

--- Apps/Bloom/DataContainer/SwiftData/ModelActors/FoodItemLogModelActor.swift
+++ Apps/Bloom/DataContainer/SwiftData/ModelActors/FoodItemLogModelActor.swift
@@ -5,6 +5,7 @@
 //  Created by Mark DiFranco on 2024-11-19.
 //
 
+import BloomFoundation
 import Foundation
 import SwiftData
 
@@ -71,15 +72,16 @@ public extension FoodItemLogModelActor {
         return try context.fetch(descriptor).map { $0.asDTO() }
     }
 
-  func fetchFrequentLogs(for meal: FoodItemLog.Meal, limit: Int? = nil) throws -> [FoodItemLogDTO] {
-    // Calculate 2 months ago.
-    guard let twoMonthsAgo = Calendar.current.date(byAdding: .month, value: -2, to: Date()) else {
-      return []
-    }
-
+  func fetchFrequentLogs(
+    for meal: FoodItemLog.Meal,
+    dateRange: DateRange = DateRange.trailingMonthsFromNow(2)
+  ) throws -> [FoodItemLogDTO] {
+    let startDate = dateRange.start
+    let endDate = dateRange.end
     let descriptor = FetchDescriptor<FoodItemLog>(
       predicate: #Predicate<FoodItemLog> { model in
-        model.date >= twoMonthsAgo
+        model.date >= startDate &&
+        model.date <= endDate
         // TODO: Zach - we cannot query enums stored in SwiftData. Their rawValue must be on the model.
         // https://forums.developer.apple.com/forums/thread/735638
         // Do this in the V6 migration.
@@ -90,14 +92,9 @@ public extension FoodItemLogModelActor {
     let logs = try context.fetch(descriptor)
 
     // Count occurrences of each food item, sort by most frequent.
-    var frequencyMap = Dictionary(grouping: logs, by: { $0.foodItem?.id })
+    let frequencyMap = Dictionary(grouping: logs, by: { $0.foodItem?.id })
       .compactMapValues { $0.count }
       .sorted { $0.value > $1.value }
-
-    // If a limit is set, apply that to the mapping.
-    if let limit {
-      frequencyMap = Array(frequencyMap.prefix(limit))
-    }
 
     // Map food logs in sorted order based on frequency.
     return frequencyMap.compactMap { foodItemID, _ in
