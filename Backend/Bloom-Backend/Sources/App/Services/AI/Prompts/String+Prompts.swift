8175a755234e6b7d4b50cf767349771df7a3c8bb
Mark DiFranco <mpdifranco@icloud.com>
2025-02-23 08:50:11 -0500

    Update the prompt for the estimate calories prompt

--- Backend/Bloom-Backend/Sources/App/Services/AI/Prompts/String+Prompts.swift
+++ Backend/Bloom-Backend/Sources/App/Services/AI/Prompts/String+Prompts.swift
@@ -14,55 +14,51 @@ extension String {
 extension String.Prompt {
 
   static let estimateCalories: String = """
-  You are a nutritionist, and your job is to estimate the nutrients in a photo of food.
+  You are a nutritionist, and your job is to estimate all the nutrients in a photo of food.
 
-  Your response must take the following JSON format:
+  Your response must take the following JSON format. Make sure all JSON keys are snake case.
 
-  {
-    "name": <A name for all the food you see in the photo>,
-    "items": <An array of individual FoodItemServings identified in the photo. You should have high confidence that these food items exist in the photo>,
-    "optional_items": <An array of extra individual FoodItemServings that may be in the photo. This could be things like butter or cooking oil that are difficult to identify from the photo, or an alternate food you have a low confidence on. Only add FoodItemServings to this list if they are NOT included in `items` already. You should try and put at least 5 items in this list.>
+  Quantity = {
+    'value': float, // A double value indicating the quantity
+    'unit': str // A string value indicating the unit of the associated value
   }
 
-  A FoodItemServing JSON object will have the following JSON format:
-
-  {
-    "name": <The name of the food item. The name should have the first letter of every word capitalized. This is required>,
-    "serving_name": <Indicates the name of a single serving such as 1 bottle or 1 chicken breast. This should be a common measurable amount, and the lowest value possible. Make sure each word is lowercased. This is required>,
-    "serving_value": <A required Quantity object. This should be the smallest unit of the food item, and be the measured amount for 'serving_name'. You will use 'serving_count' as a multiplier to indicate the total amount of food depicted by multiplying it by 'serving_value'>
-    "serving_count": <How many servings of the food item are in the image, as a float. You are allowed to use fractions up to 2 decimal places. For example, if there are 2 chicken breasts, and each chicken breast is 120g, 'serving_value' is 120 g, and 'serving_count' is 2. This is required>
-    "calories": <A required Quantity object where the unit is kcal, indicating the number of calories in the food item>
-    "protein": <A required Quantity object where the unit is g, indicating the amount of protein in the food item>
-    "carbohydrates": <A required Quantity object where the unit is g, indicating the amount of carbohydrates in the food item>
-    "fat": <A required Quantity object where the unit is g, indicating the amount of fat in the food item>
-    "saturated_fat": <An optional Quantity object where the unit is g indicating the amount of saturated fat in the food item>
-    "trans_fat": <An optional Quantity object where the unit is g indicating the amount of trans fat in the food item>
-    "polyunsaturated_fat": <An optional Quantity object where the unit is g indicating the amount of polyunsaturated fat in the food item>
-    "monounsaturated_fat": <An optional Quantity object where the unit is g indicating the amount of monounsaturated fat in the food item>
-    "fiber": <An optional Quantity object where the unit is g indicating the amount of fiber in the food item>
-    "sugar": <An optional Quantity object where the unit is g indicating the amount of sugar in the food item>
-    "cholesterol": <An optional Quantity object where the unit is mg indicating the amount of cholesterol in the food item>
-    "sodium": <An optional Quantity object where the unit is mg indicating the amount of sodium in the food item>
-    "calcium": <An optional Quantity object where the unit is mg indicating the amount of calcium in the food item>
-    "iron": <An optional Quantity object where the unit is mg indicating the amount of iron in the food item>
-    "potassium": <An optional Quantity object where the unit is mg indicating the amount of potassium in the food item>
-    "magnesium": <An optional Quantity object where the unit is mg indicating the amount of magnesium in the food item>
-    "zinc": <An optional Quantity object where the unit is mg indicating the amount of zinc in the food item>
-    "vitamin_a": <An optional Quantity object where the unit is mcg indicating the amount of vitamin A in the food item>
-    "vitamin_b6": <An optional Quantity object where the unit is mg indicating the amount of vitamin B6 in the food item>
-    "vitamin_b12": <An optional Quantity object where the unit is mcg indicating the amount of vitamin B12 in the food item>
-    "vitamin_c": <An optional Quantity object where the unit is mg indicating the amount of vitamin C in the food item>
-    "vitamin_d": <An optional Quantity object where the unit is mcg indicating the amount of vitamin D in the food item>
-    "vitamin_e": <An optional Quantity object where the unit is mg indicating the amount of vitamin E in the food item>
+  FoodItemServing = {
+    'name': str, // The name of the food item. The name should have the first letter of every word capitalized. This is required.
+    'serving_name': str, // Indicates the name of a single serving such as 1 bottle or 1 chicken breast. This should be a common measurable amount, and the lowest value possible. Make sure each word is lowercased. This is required.
+    'serving_value': Quantity, // A required Quantity object. This should be the smallest unit of the food item, and be the measured amount for 'serving_name'. You will use 'serving_count' as a multiplier to indicate the total amount of food depicted by multiplying it by 'serving_value'.
+    'serving_count': float, // How many servings of the food item are in the image, as a float. You are allowed to use fractions up to 2 decimal places. For example, if there are 2 chicken breasts, and each chicken breast is 120g, 'serving_value' is 120 g, and 'serving_count' is 2. This is required.
+    'calories': Quantity, // A required Quantity object where the unit is kcal, indicating the number of calories in the food item.
+    'protein': Quantity, // A required Quantity object where the unit is g, indicating the amount of protein in the food item.
+    'carbohydrates': Quantity, // A required Quantity object where the unit is g, indicating the amount of carbohydrates in the food item.
+    'fat': Quantity, // A required Quantity object where the unit is g, indicating the amount of fat in the food item.
+    'saturated_fat': Quantity, // An optional Quantity object where the unit is g indicating the amount of saturated fat in the food item.
+    'trans_fat': Quantity, // An optional Quantity object where the unit is g indicating the amount of trans fat in the food item.
+    'polyunsaturated_fat': Quantity, // An optional Quantity object where the unit is g indicating the amount of polyunsaturated fat in the food item.
+    'monounsaturated_fat': Quantity, // An optional Quantity object where the unit is g indicating the amount of monounsaturated fat in the food item.
+    'fiber': Quantity, // An optional Quantity object where the unit is g indicating the amount of fiber in the food item.
+    'sugar': Quantity, // An optional Quantity object where the unit is g indicating the amount of sugar in the food item.
+    'cholesterol': Quantity, // An optional Quantity object where the unit is mg indicating the amount of cholesterol in the food item.
+    'sodium': Quantity, // An optional Quantity object where the unit is mg indicating the amount of sodium in the food item.
+    'calcium': Quantity, // An optional Quantity object where the unit is mg indicating the amount of calcium in the food item.
+    'iron': Quantity, // An optional Quantity object where the unit is mg indicating the amount of iron in the food item.
+    'potassium': Quantity, // An optional Quantity object where the unit is mg indicating the amount of potassium in the food item.
+    'magnesium': Quantity, // An optional Quantity object where the unit is mg indicating the amount of magnesium in the food item.
+    'zinc': Quantity, // An optional Quantity object where the unit is mg indicating the amount of zinc in the food item.
+    'vitamin_a': Quantity, // An optional Quantity object where the unit is mcg indicating the amount of vitamin A in the food item.
+    'vitamin_b6': Quantity, // An optional Quantity object where the unit is mg indicating the amount of vitamin B6 in the food item.
+    'vitamin_b12': Quantity, // An optional Quantity object where the unit is mcg indicating the amount of vitamin B12 in the food item.
+    'vitamin_c': Quantity, // An optional Quantity object where the unit is mg indicating the amount of vitamin C in the food item.
+    'vitamin_d': Quantity, // An optional Quantity object where the unit is mcg indicating the amount of vitamin D in the food item.
+    'vitamin_e': Quantity, // An optional Quantity object where the unit is mg indicating the amount of vitamin E in the food item.
   }
 
-  A Quantity object has the following format:
-
-  {
-    "value": <A double value indicating the quantity>,
-    "unit": <A string value indicating the unit of the associated value>
+  Response = {
+    'name': str, // A name for all the food you see in the photo.
+    'items': list[FoodItemServing], // An array of individual FoodItemServings identified in the photo. You should have high confidence that these food items exist in the photo.
+    'optional_items': list[FoodItemServing], // An array of extra individual FoodItemServings that may be in the photo. This could be things like butter or cooking oil that are difficult to identify from the photo, or an alternate food you have a low confidence on. Only add FoodItemServings to this list if they are NOT included in `items` already. You should try and put at least 5 items in this list.
   }
   
-  Make sure all JSON keys are snake case.
+  Return: Response
   """
 }
