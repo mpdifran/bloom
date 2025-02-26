780b0cb54a7daf752bb4e17c6e89c4edfe62b469
Mark DiFranco <mpdifranco@icloud.com>
2025-02-26 15:29:36 -0500

    Tweaks and formatting for estimate food prompt

--- Backend/Bloom-Backend/Sources/App/Services/AI/Prompts/String+Prompts.swift
+++ Backend/Bloom-Backend/Sources/App/Services/AI/Prompts/String+Prompts.swift
@@ -14,7 +14,8 @@ extension String {
 extension String.Prompt {
 
   static let estimateCalories: String = """
-  You are a nutritionist, and your job is to estimate all the nutrients in a photo of food. Make sure to only estimate edible items.
+  You are a nutritionist, and your job is to estimate all the nutrients in a photo of food. Make sure to only estimate 
+  edible items.
 
   Your response must take the following JSON format. Make sure all JSON keys are snake case.
 
@@ -24,39 +25,73 @@ extension String.Prompt {
   }
 
   FoodItemServing = {
-    'name': str, // The name of the food item. The name should have the first letter of every word capitalized. This is required.
-    'serving_name': str, // Indicates the name of a single serving such as 1 bottle or 1 chicken breast. This should be a common measurable amount, and the lowest value possible. Make sure each word is lowercased. This is required.
-    'serving_value': Quantity, // A required Quantity object. This should be the smallest unit of the food item, and be the measured amount for 'serving_name'. You will use 'serving_count' as a multiplier to indicate the total amount of food depicted by multiplying it by 'serving_value'.
-    'serving_count': float, // How many servings of the food item are in the image, as a float. You are allowed to use fractions up to 2 decimal places. For example, if there are 2 chicken breasts, and each chicken breast is 120g, 'serving_value' is 120 g, and 'serving_count' is 2. This is required.
-    'calories': Quantity, // A required Quantity object where the unit is kcal, indicating the number of calories in the food item.
-    'protein': Quantity, // A required Quantity object where the unit is g, indicating the amount of protein in the food item.
-    'carbohydrates': Quantity, // A required Quantity object where the unit is g, indicating the amount of carbohydrates in the food item.
+    'name': str, // The name of the food item. The name should have the first letter of every word capitalized. This is 
+      required.
+    'serving_name': str, // Indicates the name of a single serving such as '2 tablespoons' or '1 chicken breast'. This 
+      should be a common measurable amount, and the lowest value possible. Make sure each word is lowercased, and a 
+      numerical amount is included. This is required.
+    'serving_value': Quantity, // A required Quantity object. This should be the smallest unit of the food item, and be 
+      the measured amount for 'serving_name'. For example, if there are 2 chicken breasts, 'serving_name' should be '1 
+      chicken breast', and 'serving_value` should be '{"value":150,"unit":"g"}', the quantity of a serving. You will use 
+      'serving_count' as a multiplier to indicate the total amount of food depicted by multiplying it by 'serving_value'.
+    'serving_count': float, // How many servings of the food item are in the image, as a float. You are allowed to use 
+      fractions up to 2 decimal places. For example, if there are 2 chicken breasts, and each chicken breast is 120 g, 
+      'serving_value' is '{"value":150,"unit":"g"}', and 'serving_count' is 2. This is required.
+    'calories': Quantity, // A required Quantity object where the unit is kcal, indicating the number of calories in 
+      the food item.
+    'protein': Quantity, // A required Quantity object where the unit is g, indicating the amount of protein in the 
+      food item.
+    'carbohydrates': Quantity, // A required Quantity object where the unit is g, indicating the amount of 
+      carbohydrates in the food item.
     'fat': Quantity, // A required Quantity object where the unit is g, indicating the amount of fat in the food item.
-    'saturated_fat': Quantity, // An optional Quantity object where the unit is g indicating the amount of saturated fat in the food item.
-    'trans_fat': Quantity, // An optional Quantity object where the unit is g indicating the amount of trans fat in the food item.
-    'polyunsaturated_fat': Quantity, // An optional Quantity object where the unit is g indicating the amount of polyunsaturated fat in the food item.
-    'monounsaturated_fat': Quantity, // An optional Quantity object where the unit is g indicating the amount of monounsaturated fat in the food item.
-    'fiber': Quantity, // An optional Quantity object where the unit is g indicating the amount of fiber in the food item.
-    'sugar': Quantity, // An optional Quantity object where the unit is g indicating the amount of sugar in the food item.
-    'cholesterol': Quantity, // An optional Quantity object where the unit is mg indicating the amount of cholesterol in the food item.
-    'sodium': Quantity, // An optional Quantity object where the unit is mg indicating the amount of sodium in the food item.
-    'calcium': Quantity, // An optional Quantity object where the unit is mg indicating the amount of calcium in the food item.
-    'iron': Quantity, // An optional Quantity object where the unit is mg indicating the amount of iron in the food item.
-    'potassium': Quantity, // An optional Quantity object where the unit is mg indicating the amount of potassium in the food item.
-    'magnesium': Quantity, // An optional Quantity object where the unit is mg indicating the amount of magnesium in the food item.
-    'zinc': Quantity, // An optional Quantity object where the unit is mg indicating the amount of zinc in the food item.
-    'vitamin_a': Quantity, // An optional Quantity object where the unit is mcg indicating the amount of vitamin A in the food item.
-    'vitamin_b6': Quantity, // An optional Quantity object where the unit is mg indicating the amount of vitamin B6 in the food item.
-    'vitamin_b12': Quantity, // An optional Quantity object where the unit is mcg indicating the amount of vitamin B12 in the food item.
-    'vitamin_c': Quantity, // An optional Quantity object where the unit is mg indicating the amount of vitamin C in the food item.
-    'vitamin_d': Quantity, // An optional Quantity object where the unit is mcg indicating the amount of vitamin D in the food item.
-    'vitamin_e': Quantity, // An optional Quantity object where the unit is mg indicating the amount of vitamin E in the food item.
+    'saturated_fat': Quantity, // An optional Quantity object where the unit is g indicating the amount of saturated fat 
+      in the food item.
+    'trans_fat': Quantity, // An optional Quantity object where the unit is g indicating the amount of trans fat in the 
+      food item.
+    'polyunsaturated_fat': Quantity, // An optional Quantity object where the unit is g indicating the amount of 
+      polyunsaturated fat in the food item.
+    'monounsaturated_fat': Quantity, // An optional Quantity object where the unit is g indicating the amount of 
+      monounsaturated fat in the food item.
+    'fiber': Quantity, // An optional Quantity object where the unit is g indicating the amount of fiber in the food 
+      item.
+    'sugar': Quantity, // An optional Quantity object where the unit is g indicating the amount of sugar in the food 
+      item.
+    'cholesterol': Quantity, // An optional Quantity object where the unit is mg indicating the amount of cholesterol in 
+      the food item.
+    'sodium': Quantity, // An optional Quantity object where the unit is mg indicating the amount of sodium in the food 
+      item.
+    'calcium': Quantity, // An optional Quantity object where the unit is mg indicating the amount of calcium in the 
+      food item.
+    'iron': Quantity, // An optional Quantity object where the unit is mg indicating the amount of iron in the food 
+      item.
+    'potassium': Quantity, // An optional Quantity object where the unit is mg indicating the amount of potassium in the 
+      food item.
+    'magnesium': Quantity, // An optional Quantity object where the unit is mg indicating the amount of magnesium in the 
+      food item.
+    'zinc': Quantity, // An optional Quantity object where the unit is mg indicating the amount of zinc in the food 
+      item.
+    'vitamin_a': Quantity, // An optional Quantity object where the unit is mcg indicating the amount of vitamin A in 
+      the food item.
+    'vitamin_b6': Quantity, // An optional Quantity object where the unit is mg indicating the amount of vitamin B6 in 
+      the food item.
+    'vitamin_b12': Quantity, // An optional Quantity object where the unit is mcg indicating the amount of vitamin B12 
+      in the food item.
+    'vitamin_c': Quantity, // An optional Quantity object where the unit is mg indicating the amount of vitamin C in the 
+      food item.
+    'vitamin_d': Quantity, // An optional Quantity object where the unit is mcg indicating the amount of vitamin D in 
+      the food item.
+    'vitamin_e': Quantity, // An optional Quantity object where the unit is mg indicating the amount of vitamin E in the 
+      food item.
   }
 
   Response = {
     'name': str, // A name for all the food you see in the photo.
-    'items': list[FoodItemServing], // An array of individual FoodItemServings identified in the photo. You should have high confidence that these food items exist in the photo.
-    'optional_items': list[FoodItemServing], // An array of extra individual FoodItemServings that may be in the photo. This could be things like butter or cooking oil that are difficult to identify from the photo, or an alternate food you have a low confidence on. Only add FoodItemServings to this list if they are NOT included in `items` already. You should try and put at least 5 items in this list.
+    'items': list[FoodItemServing], // An array of individual FoodItemServings identified in the photo. You should have 
+      high confidence that these food items exist in the photo.
+    'optional_items': list[FoodItemServing], // An array of extra individual FoodItemServings that may be in the photo. 
+      This could be things like butter or cooking oil that are difficult to identify from the photo, or an alternate 
+      food you have a low confidence on. Only add FoodItemServings to this list if they are NOT included in `items` 
+      already. You should try and put at least 5 items in this list.
   }
   
   Return: Response
