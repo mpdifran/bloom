26de9d9ebe3f69bba04b85f1d3ae87df7137137c
Zach Radford <zach.radford@faire.com>
2025-03-24 20:09:48 -0400

    create base project

---
+++ Demo/SwiftData-Repro/SwiftData-Repro/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SwiftData-Repro
+//
+//  Created by Zach Radford on 2025-03-24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
