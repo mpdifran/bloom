801c296724f96cccef259f50b93f5e4434287378
Zach Radford <zach.radford@faire.com>
2025-03-24 20:47:29 -0400

    use container

--- Demo/SwiftData-Repro/SwiftData-Repro/DataContainer/ContainerHolder.swift
+++ Demo/SwiftData-Repro/SwiftData-Repro/DataContainer/ContainerHolder.swift
@@ -11,5 +11,22 @@ import SwiftData
 final class ContainerHolder: Sendable {
   static let shared = ContainerHolder()
 
+  public let container: ModelContainer
 
+  private init() {
+    let schema = Schema(versionedSchema: currentSchema)
+    let modelConfiguration = ModelConfiguration(
+      schema: schema
+    )
+
+    do {
+      self.container = try ModelContainer(
+        for: schema,
+        migrationPlan: DefaultMigrationPlan.self,
+        configurations: modelConfiguration
+      )
+    } catch {
+      fatalError("Could not set up model container: \(modelConfiguration.url.absoluteString)\n\nError: \(error.localizedDescription)")
+    }
+  }
 }
--- Demo/SwiftData-Repro/SwiftData-Repro/SwiftData_ReproApp.swift
+++ Demo/SwiftData-Repro/SwiftData-Repro/SwiftData_ReproApp.swift
@@ -10,23 +10,10 @@ import SwiftData
 
 @main
 struct SwiftData_ReproApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-        .modelContainer(sharedModelContainer)
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+    .modelContainer(ContainerHolder.shared.container)
+  }
 }
