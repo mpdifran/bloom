36c4b5a1773b43f98c6a80a1c159f1047ca79106
Mark DiFranco <mpdifranco@icloud.com>
2025-04-14 09:44:08 -0400

    Update to vital bar view style

--- Apps/Bloom/Bloom/UserInterface/Vitals/Details/Components/VitalStatusBarView.swift
+++ Apps/Bloom/Bloom/UserInterface/Vitals/Details/Components/VitalStatusBarView.swift
@@ -9,106 +9,110 @@ import SwiftUI
 import DataContainer
 
 private extension CGFloat {
-    static let cornerRadius: CGFloat = 5
-    static let expandedRectangleWidth: CGFloat = 30
-    static let compactRectangleWidth: CGFloat = 10
-    static let rectangleHeight: CGFloat = 10
-    static let dotInternalPadding: CGFloat = 5
-    static let circleDiameter: CGFloat = 6
+  static let expandedRectangleWidth: CGFloat = 30
+  static let rectangleHeight: CGFloat = 14
+  static let circleDiameter: CGFloat = 10
 }
 
 private extension Double {
-    static let colorFillOpacity: CGFloat = 0.4
+  static let colorFillOpacity: CGFloat = 0.4
 }
 
 extension VitalStatusBarView {
-    enum Level: CaseIterable, Identifiable {
-        var id: Self { self }
+  enum Level: CaseIterable, Identifiable {
+    var id: Self { self }
 
-        case low
-        case medium
-        case high
-        case optimal
+    case low
+    case medium
+    case high
+    case optimal
 
-        init(barLevel: VitalModel.Level) {
-            switch barLevel {
-            case .low: self = .low
-            case .medium: self = .medium
-            case .high: self = .high
-            case .optimal: self = .optimal
-            @unknown default: fatalError("Unhandled Case")
-            }
-        }
-
-        var color: Color {
-            switch self {
-            case .low:
-                    .vitalSevere
-            case .medium:
-                    .vitalWarning
-            case .high:
-                    .vitalGood
-            case .optimal:
-                    .vitalGreat
-            }
-        }
+    init(barLevel: VitalModel.Level) {
+      switch barLevel {
+      case .low: self = .low
+      case .medium: self = .medium
+      case .high: self = .high
+      case .optimal: self = .optimal
+      @unknown default: fatalError("Unhandled Case")
+      }
     }
+
+    var color: Color {
+      switch self {
+      case .low:
+          .vitalSevere
+      case .medium:
+          .vitalWarning
+      case .high:
+          .vitalGood
+      case .optimal:
+          .vitalGreat
+      }
+    }
+  }
 }
 
 struct VitalStatusBarView: View {
-    let level: Level?
-    let levelPercent: Double?
+  let level: Level?
+  let levelPercent: Double?
 
-    var body: some View {
-        HStack(spacing: 2) {
-            ForEach(Level.allCases) { levelCase in
-                createRectangleShape(for: levelCase)
-                    .frame(width: level == levelCase ? .expandedRectangleWidth : .compactRectangleWidth)
-                    .overlay {
-                        if level == levelCase, let clampedPercent {
-                            HStack {
-                                Circle()
-                                    .fill(levelCase.color)
-                                    .frame(square: .circleDiameter)
-                                    .padding(.leading, ((.expandedRectangleWidth - 2 * .dotInternalPadding) * clampedPercent) - (.circleDiameter / 2))
-                                    .transition(.scale)
-                                Spacer(minLength: 0)
-                            }
-                            .frame(width: .expandedRectangleWidth - (2 * .dotInternalPadding))
-                        }
-                    }
+  var body: some View {
+    HStack(spacing: 2) {
+      ForEach(Level.allCases) { levelCase in
+        createRectangleShape(for: levelCase)
+          .frame(width: level == levelCase ? .expandedRectangleWidth : .rectangleHeight)
+          .overlay {
+            if level == levelCase, let clampedPercent {
+              HStack {
+                Circle()
+                  .fill(levelCase.color)
+                  .frame(square: .circleDiameter)
+                  .padding(.leading, leadingPadding(for: clampedPercent))
+                  .transition(.scale)
+                Spacer(minLength: 0)
+              }
+              .frame(width: .expandedRectangleWidth)
             }
-        }
-        .frame(height: .rectangleHeight)
-        .animation(.bouncy, value: level)
-        .animation(.bouncy, value: levelPercent)
+          }
+      }
     }
+    .frame(height: .rectangleHeight)
+    .animation(.bouncy, value: level)
+    .animation(.bouncy, value: levelPercent)
+  }
 }
 
 private extension VitalStatusBarView {
 
-    var clampedPercent: Double? {
-        guard let levelPercent else { return nil }
+  var clampedPercent: Double? {
+    guard let levelPercent else { return nil }
 
-        return max(min(1, levelPercent), 0)
-    }
+    return max(min(1, levelPercent), 0)
+  }
 
-    func createRectangleShape(for level: Level) -> some View {
-        RoundedRectangle(cornerRadius: .cornerRadius)
-            .fill(level.color.opacity(.colorFillOpacity))
-            .overlay {
-                RoundedRectangle(cornerRadius: .cornerRadius)
-                    .stroke(level.color)
-            }
-    }
+  func leadingPadding(for clampedPercent: Double) -> CGFloat {
+    let widthMinusPadding = CGFloat.expandedRectangleWidth - (2 * dotInternalPadding) - .circleDiameter
+    let percentageWidth = widthMinusPadding * clampedPercent
+
+    return percentageWidth + dotInternalPadding
+  }
+
+  var dotInternalPadding: CGFloat {
+    (CGFloat.rectangleHeight - CGFloat.circleDiameter) / 2
+  }
+
+  func createRectangleShape(for level: Level) -> some View {
+    Capsule()
+      .fill(level.color.tertiary)
+  }
 }
 
 #Preview {
-    VStack {
-        VitalStatusBarView(level: .low, levelPercent: 0.5)
-        VitalStatusBarView(level: .medium, levelPercent: 0)
-        VitalStatusBarView(level: .high, levelPercent: 1)
-        VitalStatusBarView(level: .optimal, levelPercent: 0.75)
-        VitalStatusBarView(level: nil, levelPercent: nil)
-    }
+  VStack {
+    VitalStatusBarView(level: .low, levelPercent: 0.5)
+    VitalStatusBarView(level: .medium, levelPercent: 0)
+    VitalStatusBarView(level: .high, levelPercent: 1)
+    VitalStatusBarView(level: .optimal, levelPercent: 0.75)
+    VitalStatusBarView(level: nil, levelPercent: nil)
+  }
 }
--- Apps/Bloom/Bloom/UserInterface/Vitals/MonthlyVitalCardCell.swift
+++ Apps/Bloom/Bloom/UserInterface/Vitals/MonthlyVitalCardCell.swift
@@ -66,52 +66,55 @@ struct MonthlyVitalCardCell: View {
 }
 
 #Preview {
-  ScrollView {
-    VStack {
-      MonthlyVitalCardCell(
-        vital: VitalModel(
-          id: .activityLevel,
-          subtitle: "Basal: 1756 Cal\nActive: 642 Cal",
-          status: "Moderate",
-          color: .vitalWarning,
-          barLevel: VitalModel.BarLevel(level: .medium, proportion: 1),
-          hasNoData: false
+  PreviewEnvironment {
+    ScrollView {
+      VStack {
+        MonthlyVitalCardCell(
+          vital: VitalModel(
+            id: .activityLevel,
+            subtitle: "Basal: 1756 Cal\nActive: 642 Cal",
+            status: "Moderate",
+            color: .vitalWarning,
+            barLevel: VitalModel.BarLevel(level: .medium, proportion: 1),
+            hasNoData: false
+          )
         )
-      )
 
-      MonthlyVitalCardCell(
-        vital: VitalModel(
-          id: .sleepQuality,
-          subtitle: "45% Core\n12% Deep",
-          status: "Good",
-          color: .vitalGreat,
-          barLevel: VitalModel.BarLevel(level: .optimal, proportion: 0.9),
-          hasNoData: false
+        MonthlyVitalCardCell(
+          vital: VitalModel(
+            id: .sleepQuality,
+            subtitle: "45% Core\n12% Deep",
+            status: "Good",
+            color: .vitalGreat,
+            barLevel: VitalModel.BarLevel(level: .optimal, proportion: 0.9),
+            hasNoData: false
+          )
         )
-      )
 
-      MonthlyVitalCardCell(
-        vital: VitalModel(
-          id: .activityLevel,
-          subtitle: "1700 Cal Basal\n451 Cal Active",
-          status: "Light",
-          color: .vitalGood,
-          barLevel: VitalModel.BarLevel(level: .high, proportion: 0.6),
-          hasNoData: false
+        MonthlyVitalCardCell(
+          vital: VitalModel(
+            id: .activityLevel,
+            subtitle: "1700 Cal Basal\n451 Cal Active",
+            status: "Light",
+            color: .vitalGood,
+            barLevel: VitalModel.BarLevel(level: .high, proportion: 0.6),
+            hasNoData: false
+          )
         )
-      )
 
-      MonthlyVitalCardCell(
-        vital: VitalModel(
-          id: .bowelMovements,
-          subtitle: "Once a Day",
-          status: "Irregular",
-          color: .vitalSevere,
-          barLevel: VitalModel.BarLevel(level: .low, proportion: 0.4),
-          hasNoData: false
+        MonthlyVitalCardCell(
+          vital: VitalModel(
+            id: .bowelMovements,
+            subtitle: "Once a Day",
+            status: "Irregular",
+            color: .vitalSevere,
+            barLevel: VitalModel.BarLevel(level: .low, proportion: 0.4),
+            hasNoData: false
+          )
         )
-      )
+      }
+      .padding()
     }
-    .padding()
+    .groupedBackground()
   }
 }
