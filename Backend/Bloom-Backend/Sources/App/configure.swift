7249bbce0c46776becc6ae51bcf7eb201d513e07
Mark DiFranco <mpdifranco@icloud.com>
2025-05-10 12:32:11 -0400

    Pull out migrations to ensure app launches in time

--- Backend/Bloom-Backend/Sources/App/configure.swift
+++ Backend/Bloom-Backend/Sources/App/configure.swift
@@ -9,11 +9,17 @@ public func configure(_ app: Application) async throws {
   // TelemetryDeck
   //    app.telemetryDeck.initialize(appID: "F1AC4445-7F73-4026-A19A-FF2250C34853")
 
+  app.logger.info("1")
+
   // Debug
   app.printEnvironmentInfo()
 
+  app.logger.info("2")
+
   // Redis
   try app.setupRedis()
+
+  app.logger.info("3")
 
   // Middleware
 
@@ -21,11 +27,19 @@ public func configure(_ app: Application) async throws {
   app.routes.defaultMaxBodySize = "10mb"
   try routes(app)
 
+  app.logger.info("4")
+
   // Database
   try app.setupPostgres()
-  allMigrations.forEach { app.migrations.add($0) }
-  try await app.autoMigrate() // Perform migration
+
+  // Migrations are done in the release phase on Postgres
+//  allMigrations.forEach { app.migrations.add($0) }
+//  try await app.autoMigrate() // Perform migration
+
+  app.logger.info("5")
 
   // APNs
   try app.configureAPNs()
+
+  app.logger.info("6")
 }
