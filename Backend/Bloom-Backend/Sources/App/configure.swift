a672291b39be20fc7a53726782cfde7246a7314a
Mark DiFranco <mpdifranco@icloud.com>
2025-05-12 15:54:15 -0400

    Revert everything to normal to test the 603-release buildpack

--- Backend/Bloom-Backend/Sources/App/configure.swift
+++ Backend/Bloom-Backend/Sources/App/configure.swift
@@ -9,17 +9,11 @@ public func configure(_ app: Application) async throws {
   // TelemetryDeck
   //    app.telemetryDeck.initialize(appID: "F1AC4445-7F73-4026-A19A-FF2250C34853")
 
-  app.logger.info("1")
-
   // Debug
   app.printEnvironmentInfo()
 
-  app.logger.info("2")
-
   // Redis
   try app.setupRedis()
-
-  app.logger.info("3")
 
   // Middleware
 
@@ -27,19 +21,10 @@ public func configure(_ app: Application) async throws {
   app.routes.defaultMaxBodySize = "10mb"
   try routes(app)
 
-  app.logger.info("4")
-
   // Database
   try app.setupPostgres()
-  app.logger.info("4.1")
   allMigrations.forEach { app.migrations.add($0) }
-//  app.logger.info("4.2")
-//  try await app.autoMigrate() // Perform migration
-
-  app.logger.info("5")
 
   // APNs
   try app.configureAPNs()
-
-  app.logger.info("6")
 }
