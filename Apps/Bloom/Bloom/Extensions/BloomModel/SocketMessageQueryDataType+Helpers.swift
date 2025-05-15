6f78cfe385b565767b61c2b69ee636ddc9a06a62
Mark DiFranco <mpdifranco@icloud.com>
2025-05-15 16:18:52 -0400

    Fix build issues

--- Apps/Bloom/Bloom/Extensions/BloomModel/SocketMessageQueryDataType+Helpers.swift
+++ Apps/Bloom/Bloom/Extensions/BloomModel/SocketMessageQueryDataType+Helpers.swift
@@ -23,6 +23,22 @@ extension SocketMessage.QueryDataType {
     case .stress: "stress"
     case .workouts: "workouts"
     case .targetHeartRateZoneMinutes: "target heart rate zones"
+    case .caloricIntake: "caloric intake"
+    case .proteinIntake: "protein intake"
+    case .waterIntake: "water intake"
+    case .fiberIntake: "fiber intake"
+    case .meditationMinutes: "meditation minutes"
+    case .exerciseMinutes: "exercise minutes"
+    case .stepCount: "step count"
+    case .walkingRunningDistance: "walking running distance"
+    case .runDistance: "run distance"
+    case .runDuration: "run duration"
+    case .bikeDistance: "bike distance"
+    case .bikeDuration: "bike duration"
+    case .mobilityAndFlexibilityDuration: "mobility and flexibility duration"
+    case .strengthTrainingDuration: "strength training duration"
+    case .cardioDuration: "cardio duration"
+    case .highIntensityIntervalTrainingDuration: "high intensity interval training duration"
     }
   }
 }
