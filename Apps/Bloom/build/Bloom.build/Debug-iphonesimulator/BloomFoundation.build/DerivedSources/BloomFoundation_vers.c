e332f9f6c2de47b1b3cc9a6c102b110610122502
Mark DiFranco <mpdifranco@icloud.com>
2025-08-07 17:02:21 -0400

    Remove build folder

--- Apps/Bloom/build/Bloom.build/Debug-iphonesimulator/BloomFoundation.build/DerivedSources/BloomFoundation_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char BloomFoundationVersionString[];
- extern const double BloomFoundationVersionNumber;
-
- const unsigned char BloomFoundationVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:BloomFoundation  PROJECT:Bloom-1" "\n";
- const double BloomFoundationVersionNumber __attribute__ ((used)) = (double)1.;
--- Apps/Bloom/build/Bloom.build/Debug-iphonesimulator/DataContainer.build/DerivedSources/DataContainer_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char DataContainerVersionString[];
- extern const double DataContainerVersionNumber;
-
- const unsigned char DataContainerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:DataContainer  PROJECT:Bloom-1" "\n";
- const double DataContainerVersionNumber __attribute__ ((used)) = (double)1.;
