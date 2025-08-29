8e73c85184ac985a5327cda2fa8d2b0c36c8fa07
Mark DiFranco <mpdifranco@icloud.com>
2025-08-29 08:54:05 -0400

    Add today calendar cell

---
+++ Apps/Bloom/Bloom/UserInterface/Today/Cells/CalendarTodayCell.swift
@@ -0,0 +1,83 @@
+//
+//  CalendarTodayCell.swift
+//  Bloom
+//
+//  Created by Mark DiFranco on 2025-08-29.
+//
+
+import SwiftUI
+@preconcurrency import EventKit
+import EventKitUI
+import AppUI
+
+struct CalendarTodayCell: View {
+
+  @State private var events = [EKEvent]()
+  @State private var selectedEvent: EKEvent?
+  @State private var presentedSheet: AnyView?
+
+  var body: some View {
+    VStack(spacing: 12) {
+      if events.isEmpty {
+        noEventsView
+      }
+
+      ForEach(allDayEvents) { event in
+        AllDayEventCell(event: event)
+          .selectable()
+          .onTapGesture {
+            presentedSheet = EKEventView(event: event).asAny
+          }
+      }
+
+      if allDayEvents.isNotEmpty && nonAllDayEvents.isNotEmpty {
+        Divider()
+      }
+
+      ForEach(nonAllDayEvents) { event in
+        EventCell(event: event)
+          .selectable()
+          .onTapGesture {
+            presentedSheet = EKEventView(event: event).asAny
+          }
+      }
+    }
+    .cardContainer()
+    .sheet($presentedSheet)
+    .animation(.default, value: events.count)
+    .task {
+      await CalendarManager.shared.promptForPermission()
+      self.events = await CalendarManager.shared.eventsToday()
+    }
+  }
+}
+
+private extension CalendarTodayCell {
+
+  var allDayEvents: [EKEvent] {
+    events.filter({ $0.isAllDay })
+  }
+
+  var nonAllDayEvents: [EKEvent] {
+    events.filter({ !$0.isAllDay })
+  }
+
+  var noEventsView: some View {
+    ContentUnavailableView(
+      "No Events",
+      systemSymbol: .calendar,
+      description: Text("You have no events today.")
+    )
+    .foregroundStyle(.secondary)
+    .horizontallyCentered()
+//    .frame(height: 140)
+  }
+}
+
+#Preview {
+  PreviewEnvironment {
+    BloomScrollView {
+      CalendarTodayCell()
+    }
+  }
+}
