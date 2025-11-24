6e938ef5d5c7cc2e5a836cb4d6864ed90fec5483
Mark DiFranco <mpdifranco@icloud.com>
2025-11-24 12:10:25 -0500

    Attempt to fix sleep comparisons

--- Apps/Bloom/Bloom/HealthManager/Sleep/HealthSleepObserver.swift
+++ Apps/Bloom/Bloom/HealthManager/Sleep/HealthSleepObserver.swift
@@ -54,9 +54,9 @@ private extension HealthSleepObserver {
 
     let newLastSleepAnalysis = sleepAnalyses.last
 
-    if newLastSleepAnalysis != previousSleepAnalysis && previousSleepAnalysis != nil {
+    if newLastSleepAnalysis?.overallMinutesIncludingAwake != previousSleepAnalysis?.overallMinutesIncludingAwake && previousSleepAnalysis != nil {
       // Clear stale insights and trigger refresh when new sleep data is available
-      internalLog(.todayInsights, "newLastSleepAnalysis does not match previousSleepAnalysis, forcing refresh of content")
+      internalLog(.todayInsights, "newLastSleepAnalysis overall minutes does not match previousSleepAnalysis overall minutes, forcing refresh of content")
       await TodayInsightsManager.shared.forceRefreshContent()
     } else if previousSleepAnalysis == nil {
       internalLog(.todayInsights, "previousSleepAnalysis was nil, skipping content refresh")
